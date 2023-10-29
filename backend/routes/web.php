5c42b2bb4d5a568e24c4fd9619d14af72583688a
Khaled Alam <khaledalam.net@gmail.com>
2023-10-29 19:45:18 +0700

    feat: Disable debugging route.

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -33,7 +33,7 @@ Route::group(['middleware'=>'universal', InitializeTenancyByDomain::class,'as'=>
 })
 ->middleware(['universal', InitializeTenancyByDomain::class]);
 
-Route::get('test', [\App\Http\Controllers\TestController::class, 'index'])->middleware("web");
+//Route::get('test', [\App\Http\Controllers\TestController::class, 'index'])->middleware("web");
 
 Route::middleware(['accepted'])->prefix('panel')->group(function () {
     Route::get('/dashboard', [\App\Http\Controllers\Panel\DashboardController::class, 'index'])->name('dashboard');
