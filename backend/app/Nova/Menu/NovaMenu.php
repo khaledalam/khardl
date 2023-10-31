7104a709324bce4679b104cbe658b90290cc7f31
HassanElshazlyEida <hassanaboeata@gmail.com>
2023-10-31 20:11:27 +0200

    ➖ Removing Dependency:  nova-front-components

--- backend/app/Nova/Menu/NovaMenu.php
+++ backend/app/Nova/Menu/NovaMenu.php
@@ -6,8 +6,6 @@ use App\Nova\User;
 use Laravel\Nova\Nova;
 use PharIo\Manifest\License;
 use App\Nova\Dashboards\Main;
-use App\Nova\FQA;
-use App\Nova\Point;
 use Laravel\Nova\Menu\MenuItem;
 use Laravel\Nova\Menu\MenuSection;
 use Illuminate\Http\Request;
@@ -24,12 +22,7 @@ class NovaMenu
                         MenuItem::resource(User::class),
                     ])->icon('user')->collapsable(),
     
-                    MenuSection::make(__('Content'), [
-                        MenuItem::resource(FQA::class),
-                        MenuItem::resource(Point::class),
-                    ])
-                    ->icon('document-text')
-                    ->collapsable(),
+                    
                 ];
             })
         ];
