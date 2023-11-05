012368a7d611e49c554fcce6e0b1350ce605e223
Khaled Alam <khaledalam.net@gmail.com>
2023-11-05 23:29:24 +0700

    Revert "Add setup script, remove migrations/tenant."
    This reverts commit 80dd9b909b57a8589b63f916d9ee65b977044721.

---
+++ backend/database/migrations/tenant/2023_09_28_123041_create_payments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('transaction_id')->unique();
+            $table->decimal('amount', 15, 2);
+            $table->string('currency', 3);
+            $table->enum('status', ['pending', 'completed', 'failed', 'refunded']);
+            $table->string('method');
+            $table->timestamp('payment_date');
+            $table->json('details')->nullable();
+            $table->timestamps();
+
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
