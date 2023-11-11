3b68d8efa95d5b1d60c03a88b643327a45619d17
HassanElshazlyEida <hassanaboeata@gmail.com>
2023-11-11 11:34:47 +0200

    🐛 FIX: reset timer & spinner

--- backend/resources/landing-page/src/pages/LoginSignUp/VerificationEmail.jsx
+++ backend/resources/landing-page/src/pages/LoginSignUp/VerificationEmail.jsx
@@ -30,6 +30,7 @@ const VerificationEmail = () => {
    const [countdown, setCountdown] = useState(30)
    const [canResend, setCanResend] = useState(false)
    const [showCountdownText, setShowCountdownText] = useState(true)
+   const [spinner, setSpinner] = useState(false)
 
    const resetTimer = () => {
       setCountdown(30)
@@ -37,32 +38,28 @@ const VerificationEmail = () => {
       startTimer()
    }
 
-   console.log("user_email", user_email, user_email.length);
-
    if (user_email.length < 1) {
-       console.log("user_email", user_email, user_email.length);
-
        window.location.href = '/logout';
    }
 
    // API POST REQUEST
    const ResendCode = async (data) => {
       try {
+         setSpinner(true);
+         resetTimer()
          const response = await AxiosInstance.post(`/email/send-verify`, {
              email: user_email
          })
-
-          console.log(response);
-
-         if (response.ok) {
+         if (response.data) {
             toast.success(`${t('The code has been re-sent successfully')}`)
-            resetTimer()
+            
          } else {
             throw new Error(`${t('Code resend failed')}`)
          }
       } catch (error) {
          toast.error(`${t('Code resend failed')}`)
       }
+      setSpinner(false);
    }
 
    // API POST REQUEST
@@ -89,6 +86,7 @@ const VerificationEmail = () => {
 
    const startTimer = () => {
       const timer = setInterval(() => {
+         console.log(3);
          setCountdown((prevCountdown) => {
             if (prevCountdown === 1) {
                clearInterval(timer)
@@ -108,12 +106,9 @@ const VerificationEmail = () => {
       return timer;
    }
 
-   // useEffect(() => {
-   //    let timer = startTimer();
-   //    return () => {
-   //        clearInterval(timer);
-   //    }
-   // }, [])
+   useEffect(() => {
+      let timer = startTimer();
+   }, [])
 
    return (
       <div className='flex flex-col items-stretch justify-center pt-[40px]'>
@@ -173,6 +168,32 @@ const VerificationEmail = () => {
                                     {t('Resend the code')}
                                  </label>
                               )}
+                              {spinner && (
+                                 <div
+                                    role='status'
+                                    className='rounded-s-md  max-[860px]:rounded-b-lg max-[860px]:rounded-s-none absolute -translate-x-1/2 -translate-y-1/2 top-[44.1%] left-1/2 w-[100%] h-[100%] '
+                                 >
+                                    <div className='rounded-s-md max-[860px]:rounded-b-lg max-[860px]:rounded-s-none relative bg-black opacity-25 flex justify-center items-center w-[100%] h-[100%]'></div>
+                                    <div className='absolute -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2 '>
+                                       <svg
+                                          aria-hidden='true'
+                                          className='w-8 h-8 mr-2 text-gray-200 animate-spin fill-[var(--primary)]'
+                                          viewBox='0 0 100 101'
+                                          fill='none'
+                                          xmlns='http://www.w3.org/2000/svg'
+                                       >
+                                          <path
+                                             d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z'
+                                             fill='currentColor'
+                                          />
+                                          <path
+                                             d='M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z'
+                                             fill='currentFill'
+                                          />
+                                       </svg>
+                                    </div>
+                                 </div>
+                              )}
                            </div>
                         </form>
                      </div>
@@ -216,6 +237,7 @@ const VerificationEmail = () => {
                            </button>
                         </div>
                      </form>
+                     
                   </div>
                </div>
             </div>
