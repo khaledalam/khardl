177d8c1bc285d902935ac29ecd2c30acd131094c
Khaled Alam <khaledalam.net@gmail.com>
2023-11-12 19:38:15 +0700

    Remove frontend folder and git ignore building assets files.

--- frontend-donnot-use-this/src/config/consts.jsx
+++
@@ -1,7 +0,0 @@
-// development
-export const API_ENDPOINT = 'http://khardl:8000/api';
-export const WEBSITE_URL = 'http://khardl:8000';
-
-// live
-// export const API_ENDPOINT = 'https://khardl.com/api';
-// export const WEBSITE_URL = 'https://khardl.com';
