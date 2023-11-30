035e8e89ba3701d105ee3c19fd5f009cbe05bdc6
HassanElshazlyEida <hassanaboeata@gmail.com>
2023-11-30 17:08:01 +0200

    🐛 FIX: redirect RW to subdomain panel

--- backend/app/Http/Controllers/DashboardController.php
+++ backend/app/Http/Controllers/DashboardController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
+use App\Models\Tenant\RestaurantUser;
+
 class DashboardController extends Controller
 {
    
@@ -10,10 +12,17 @@ class DashboardController extends Controller
     public function index()
     {
         $user = Auth::user();
-        return match(true){
-            $user->isAdmin() => redirect()->route("admin.dashboard"),
-            $user->isRestaurantOwner() => redirect($user->restaurant->route('home')),
-            default => redirect()->route("home")
-        };
+        if($user->isAdmin()){
+            return  redirect()->route("admin.dashboard");
+        }elseif($user->isRestaurantOwner()){
+            $id = $user->restaurant->run(function ($tenant) {
+                return  RestaurantUser::whereHas('roles', function ($query) {
+                        $query->where('name', 'Restaurant Owner');
+                })->first()->id;
+            });
+            return redirect($user->restaurant->impersonationUrl($id,'dashboard'));
+        }else {
+            redirect()->route("home");
+        }
     }
 }
