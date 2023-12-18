3c744dfcbaa4da3e72de812d232641294867070f
HassanElshazlyEida <hassanaboeata@gmail.com>
2023-12-18 17:32:30 +0200

    🐛 FIX:

--- backend/app/Http/Requests/Tenant/CustomerLoginRequest.php
+++ backend/app/Http/Requests/Tenant/CustomerLoginRequest.php
@@ -30,7 +30,11 @@ class CustomerLoginRequest extends FormRequest
         if ($this->phone) {
             // Remove any non-digit characters
             $cleanedPhone = preg_replace('/\D/', '', $this->phone);
-            if (strlen($cleanedPhone) === 10) {
+
+            if (strlen($cleanedPhone) === 9) {
+                // If it's 10 digits, merge with '966'
+                $this->merge(['phone' => '966' .$cleanedPhone]);
+            }else if((strlen($cleanedPhone) === 10) && $cleanedPhone[0] == "0") {
                 // If it's 9 digits, merge with '966'
                 $this->merge(['phone' => '966' . substr($cleanedPhone,1)]);
             } elseif (strlen($cleanedPhone) === 12 && substr($cleanedPhone, 0, 3) === '966') {
--- backend/app/Http/Requests/Tenant/CustomerRegisterRequest.php
+++ backend/app/Http/Requests/Tenant/CustomerRegisterRequest.php
@@ -31,10 +31,15 @@ class CustomerRegisterRequest extends FormRequest
 
     protected function prepareForValidation()
     {
+
         if ($this->phone) {
             // Remove any non-digit characters
             $cleanedPhone = preg_replace('/\D/', '', $this->phone);
-            if (strlen($cleanedPhone) === 10) {
+
+            if (strlen($cleanedPhone) === 9) {
+                // If it's 10 digits, merge with '966'
+                $this->merge(['phone' => '966' .$cleanedPhone]);
+            }else if((strlen($cleanedPhone) === 10) && $cleanedPhone[0] == "0") {
                 // If it's 9 digits, merge with '966'
                 $this->merge(['phone' => '966' . substr($cleanedPhone,1)]);
             } elseif (strlen($cleanedPhone) === 12 && substr($cleanedPhone, 0, 3) === '966') {
