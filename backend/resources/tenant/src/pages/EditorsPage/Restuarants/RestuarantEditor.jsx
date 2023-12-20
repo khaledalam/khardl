cee18798234dd672be2c9838393cbf8bdd555b38
Precioussoul <habsoff@gmail.com>
2023-12-20 16:28:41 +0100

    feat: working on site-editor restuarant

--- backend/resources/tenant/src/pages/EditorsPage/Restuarants/RestuarantEditor.jsx
+++ backend/resources/tenant/src/pages/EditorsPage/Restuarants/RestuarantEditor.jsx
@@ -1,5 +1,14 @@
 import React from "react"
+import Navbar from "./components/Navbar"
 
 export const RestuarantEditor = () => {
-  return <div>RestuarantEditor</div>
+  return (
+    <div className='block'>
+      <Navbar />
+      <div className='flex bg-white h-[calc(100vh-75px)] w-full'>
+        <div className='flex-[18%] bg-white'></div>
+        <div className='flex-[82%]  bg-neutral-200'></div>
+      </div>
+    </div>
+  )
 }
---
+++ backend/resources/tenant/src/pages/EditorsPage/Restuarants/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import {FaPlay} from "react-icons/fa"
+import {IoMenuOutline} from "react-icons/io5"
+
+const Navbar = () => {
+  return (
+    <div className='h-[70px] w-full bg-white flex items-center justify-between px-8 border-b border-neutral-200'>
+      <IoMenuOutline size={42} />
+      <div className='flex items-center gap-4'>
+        <button className='btn btn-active p-3 flex items-center justify-center'>
+          <FaPlay size={22} />
+        </button>
+        <button className='btn btn-active w-[100px]'>Save</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
