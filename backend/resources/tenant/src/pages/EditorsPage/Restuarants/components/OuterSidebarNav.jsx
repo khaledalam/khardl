3a48d470e56c3991534e6459c83c95c78a201b30
Precioussoul <habsoff@gmail.com>
2023-12-24 22:46:36 +0100

    feat: working on restaurants homepage

--- backend/resources/tenant/src/pages/EditorsPage/Restuarants/components/OuterSidebarNav.jsx
+++ backend/resources/tenant/src/pages/EditorsPage/Restuarants/components/OuterSidebarNav.jsx
@@ -3,38 +3,62 @@ import homeIcon from "../../../../assets/homeIcon.svg"
 import shopIcon from "../../../../assets/shopIcon.svg"
 import deliveryIcon from "../../../../assets/bikeDeliveryIcon.svg"
 import {IoMenuOutline} from "react-icons/io5"
+import {FaLongArrowAltRight} from "react-icons/fa"
 
-const OuterSidebarNav = () => {
+const OuterSidebarNav = ({id}) => {
   return (
-    <div className='w-full h-full flex flex-col items-center justify-center gap-16'>
-      <label htmlFor='my-drawer' aria-label='close sidebar'>
+    <div className='w-full h-[100vh] flex flex-col items-center justify-between'>
+      <label htmlFor={id ? id : "my-drawer"} aria-label='close sidebar'>
         <IoMenuOutline size={42} className='text-neutral-400' />
       </label>
       <div className='w-full h-full flex flex-col items-center justify-center gap-6'>
-        <div className='flex flex-col gap-3 items-center justify-center'>
-          <div className='w-[60px] h-[50px] rounded-xl p-2 bg-neutral-100 flex items-center justify-center'>
+        <div className='w-[90%] mx-auto flex flex-row gap-3 bg-neutral-100 rounded-lg border border-[#C0D123] items-center '>
+          <div className='w-[60px] h-[50px] rounded-xl p-2  flex items-center justify-center'>
             <img src={homeIcon} alt='home' />
           </div>
           <h3 className=''>Home</h3>
         </div>
-        <div className='flex flex-col gap-3 items-center justify-center'>
-          <div className='w-[60px] h-[50px] rounded-xl p-2 bg-neutral-100 flex items-center justify-center'>
+        <div className='w-[90%] mx-auto flex flex-row gap-3 bg-neutral-100 rounded-lg border border-[#C0D123] items-center '>
+          <div className='w-[60px] h-[50px] rounded-xl p-2  flex items-center justify-center'>
             <img src={shopIcon} alt='shopping' />
           </div>
-          <h3 className=''>Shopping</h3>
+          <h3 className=''>Pick up</h3>
         </div>
-        <div className='flex flex-col gap-3 items-center justify-center'>
-          <div className='w-[60px] h-[50px] rounded-xl p-2 bg-neutral-100 flex items-center justify-center'>
+        <div className='w-[90%] mx-auto flex flex-row gap-3 bg-neutral-100 rounded-lg border border-[#C0D123] items-center '>
+          <div className='w-[60px] h-[50px] rounded-xl p-2  flex items-center justify-center'>
             <img src={deliveryIcon} alt='deliveryIcon' />
           </div>
           <h3 className=''>Delivery</h3>
         </div>
-        <button className='btn bg-neutral-100 hover:bg-neutral-100 active:bg-neutral-100'>
-          Logout
-        </button>
-        <button className='btn bg-neutral-100 hover:bg-neutral-100 active:bg-neutral-100'>
-          Language
-        </button>
+        <div
+          role='button'
+          className='w-[90%] mx-auto btn bg-neutral-100 hover:bg-neutral-100 active:bg-neutral-100 font-normal border border-[#C0D123]'
+        >
+          Login as Customer
+        </div>
+        <div
+          role='button'
+          className='w-[90%] mx-auto btn bg-neutral-100 hover:bg-neutral-100 active:bg-neutral-100 font-normal border border-[#C0D123]'
+        >
+          Login as Management Area{" "}
+        </div>
+        <div
+          role='button'
+          className='w-[90%] mx-auto btn bg-neutral-100 hover:bg-neutral-100 active:bg-neutral-100 font-normal !border !border-[#C0D123]'
+        >
+          AR{" "}
+        </div>
+      </div>
+      <div className='w-full mb-20'>
+        <div
+          role='button'
+          className='w-[90%] mx-auto btn bg-neutral-100 hover:bg-neutral-100 active:bg-neutral-100 font-normal border border-[#C0D123] flex items-center gap-3'
+        >
+          <span className=''>Logout </span>
+          <span>
+            <FaLongArrowAltRight size={20} />
+          </span>
+        </div>
       </div>
     </div>
   )
--- backend/resources/tenant/src/pages/RestuarantPage/components/Navbar.jsx
+++ backend/resources/tenant/src/pages/RestuarantPage/components/Navbar.jsx
@@ -1,24 +1,44 @@
-import React from "react"
+import React, {Fragment} from "react"
 import cartHeaderImg from "../../../assets/cartBoldIcon.svg"
 import {IoMenuOutline} from "react-icons/io5"
+import OuterSidebarNav from "../../EditorsPage/Restuarants/components/OuterSidebarNav"
 
 const NavbarRestuarant = () => {
   return (
-    <div className='w-full bg-white flex flex-row items-center justify-between px-12 py-2'>
-      <div className='w-[40px] h-[40px] bg-[#2A6E4F] rounded-lg p-1 flex items-center justify-center'>
-        <IoMenuOutline size={38} className='text-white' />
-      </div>
-      <div className='w-[50px] h-[50px] relative flex items-center justify-center'>
-        <img src={cartHeaderImg} alt={"cart"} className='' />
-        {true && (
-          <div className='absolute top-0 right-0'>
-            <div className='w-[18px] h-[18px] rounded-full p-1 bg-red-500 flex items-center justify-center'>
-              <span className='text-white font-bold text-xs'>0</span>
+    <Fragment>
+      <div className='w-full bg-white flex flex-row items-center justify-between px-12 py-2'>
+        <label
+          htmlFor='resNavHome'
+          className='w-[40px] h-[40px] drawer-button bg-[#2A6E4F] rounded-lg p-1 flex items-center justify-center'
+        >
+          <IoMenuOutline size={38} className='text-white' />
+        </label>
+        <div className='w-[50px] h-[50px] relative flex items-center justify-center'>
+          <img src={cartHeaderImg} alt={"cart"} className='' />
+          {true && (
+            <div className='absolute top-0 right-0'>
+              <div className='w-[18px] h-[18px] rounded-full p-1 bg-red-500 flex items-center justify-center'>
+                <span className='text-white font-bold text-xs'>0</span>
+              </div>
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
-    </div>
+      <div className='drawer z-50'>
+        <input id='resNavHome' type='checkbox' className='drawer-toggle' />
+        <div className='drawer-side'>
+          <label
+            htmlFor='resNavHome'
+            aria-label='close sidebar'
+            className='drawer-overlay'
+          ></label>
+          <div className='menu p-4 laptopXL:w-[25%] w-[30%] min-h-full bg-white text-base-content'>
+            {/* Sidebar content here */}
+            <OuterSidebarNav id={"resNavHome"} />
+          </div>
+        </div>
+      </div>
+    </Fragment>
   )
 }
 
