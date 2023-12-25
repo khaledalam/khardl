6ceca62e41d184241b9a99e75b90d4ab039e1c26
Khaled Alam <khaledalam.net@gmail.com>
2023-12-25 18:06:54 +0700

    wip: webhook logic.

---
+++ backend/app/Http/Controllers/DeliveryController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use App\Models\Tenant\RestaurantUser;
+
+class DeliveryController extends Controller
+{
+    public function webhook()
+    {
+
+    }
+}
