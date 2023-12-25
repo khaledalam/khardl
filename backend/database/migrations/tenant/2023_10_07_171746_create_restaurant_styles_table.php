09ca98564b92b9b4ce0efa25a2165647beab77ef
sezohessen <sezohessen2000@gmail.com>
2023-12-25 21:54:56 +0200

    💥 NEW: refactor restaurant style

--- backend/database/migrations/tenant/2023_10_07_171746_create_restaurant_styles_table.php
+++ backend/database/migrations/tenant/2023_10_07_171746_create_restaurant_styles_table.php
@@ -14,25 +14,35 @@ return new class extends Migration
         Schema::create('restaurant_styles', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id')->nullable(); // Ensure one style per user
-            $table->string('logo');
             $table->string('logo_alignment');
-            $table->string('category_style');
-            $table->string('banner_style');
+            $table->string('logo_shape');
+            $table->string('banner_type');
+            $table->string('banner_shape');
+            $table->string('banner_background_color');
+            $table->string('category_shape');
+            $table->string('category_hover_color');
+            $table->string('category_alignment');
+            $table->string('categoryDetail_type');
+            $table->string('categoryDetail_alignment');
+            $table->string('categoryDetail_shape');
+            $table->string('categoryDetail_cart_color');
+            $table->string('phoneNumber');
+            $table->string('phoneNumber_alignment');
+            $table->string('page_color');
+            $table->string('page_category_color');
+            $table->string('header_color');
+            $table->string('footer_color');
+            $table->string('price_color');
+            $table->json('selectedSocialIcons');
+            $table->string('text_fontFamily');
+            $table->string('text_fontWeight');
+            $table->string('text_fontSize');
+            $table->string('text_alignment');
+            $table->string('text_color');
+
+            $table->string('logo')->nullable();
             $table->string('banner_image')->nullable();
             $table->json('banner_images')->nullable();
-            $table->json('social_medias');
-            $table->string('phone_number');
-            $table->string('primary_color');
-            $table->string('buttons_style');
-            $table->string('images_style');
-            $table->string('font_family');
-            $table->string('font_type');
-            $table->string('font_size');
-            $table->string('font_alignment');
-            $table->json('left_side_button');
-            $table->json('right_side_button');
-            $table->json('center_side_button');
-
 
             $table->timestamps();
 
