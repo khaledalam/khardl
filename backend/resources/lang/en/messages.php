5f96f90939d255984edeab299c02eaf3c93115b6
Khaled Alam <khaledalam.net@gmail.com>
2023-12-27 06:11:47 +0700

    feat: Add base details of yeswa.

--- backend/resources/lang/en/messages.php
+++ backend/resources/lang/en/messages.php
@@ -373,7 +373,26 @@ return [
     'others' => "Others",
     "blocked" => "Blocked",
     "blocked-user" => "User is blocked",
-    "Bank Account"=>"Bank Account"
+    "Bank Account"=>"Bank Account",
+    "delivery.yeswa.summary" => "We are a Saudi logistics services provider located in the city of Riyadh and serve all cities across the Saudi Arabia.<br /><br />We help local businesses achieve their eCommerce potential by providing shipping and fulfillment services at competitive prices and excellent service guarantee. Currently, we serve various sectors across the Kingdom, from food services, pharmaceuticals, and fashion, to cosmetics, and electronics.",
+    "yeswa" => "Yeswa",
+    "brief" => "Brief",
+    "activation-within" => "Activation within",
+    "hour" => "Hour",
+    "coverage" => "Coverage",
+    "prices" => "Prices",
+    "payment" => "Payment",
+    "contract" => "Contract",
+    "cover-area" => "Covers the following cities",
+    "Riyadh" => "Riyadh",
+    "Jeddah" => "Jeddah",
+    "Mecca" => "Mecca",
+    "Dammam" => "Dammam",
+    "Al-Ahsa" => "Al-Ahsa",
+    "cost" => "Cost",
+    "main-cost" => "Main cost",
+    "additional-cost" => "Main cost",
+    "na" => "N/A"
 
 
 ];
