d8bca8f25a8e266f10f3fc248cb6583eef89b26c
HassanElshazlyEida <hassanaboeata@gmail.com>
2024-01-02 23:46:11 +0200

    🐛 FIX: connection sync webhook

--- backend/app/Packages/DeliveryCompanies/Webhook/DeliveryCompaniesWebhookHandler.php
+++ backend/app/Packages/DeliveryCompanies/Webhook/DeliveryCompaniesWebhookHandler.php
@@ -1,16 +1,18 @@
 <?php
 
-namespace App\Packages\TapPayment\Webhook;
+namespace App\Packages\DeliveryCompanies\Webhook;
 
+
+use Illuminate\Support\Facades\Config;
 use Spatie\WebhookClient\Jobs\ProcessWebhookJob;
 
 class DeliveryCompaniesWebhookHandler extends ProcessWebhookJob
 {
+    public $connection = 'sync';
     public function handle()
     {
         logger($this->webhookCall);
         $data = json_decode($this->webhookCall, true)['payload'];
         logger($data);
-       
     }
 }
