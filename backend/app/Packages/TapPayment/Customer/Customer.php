cabb80aa64b630850b2843fddb96402c68d1e702
HassanElshazlyEida <hassanaboeata@gmail.com>
2024-01-03 19:59:24 +0200

    💥 NEW: create Tap account for customers

--- backend/app/Packages/TapPayment/Customer/Customer.php
+++ backend/app/Packages/TapPayment/Customer/Customer.php
@@ -2,6 +2,8 @@
 
 namespace App\Packages\TapPayment\Customer;
 
+use App\Models\Tenant\RestaurantUser;
+use App\Utils\ResponseHelper;
 use App\Packages\TapPayment\Tap;
 use App\Packages\TapPayment\Customer\CustomerInterface;
 
@@ -14,5 +16,26 @@ class Customer extends Tap implements CustomerInterface
     }
     public static function retrieve(string $customer_id): array {
         return self::send("/customers/$customer_id",[],'get');
+    }
+    public static function createWithModel(RestaurantUser $user) {
+        if(env('APP_ENV') != 'local'){
+            $customer = self::create([
+                "first_name"=> $user->first_name,
+                "last_name"=> $user->last_name,
+                "email"=> $user->email,
+                "phone"=> [
+                    "country_code"=> "966",
+                    "number"=> substr($user->phone, 3)
+                ]
+            ]);
+            if($customer['http_code'] == ResponseHelper::HTTP_OK){
+                $user->tap_customer_id = $customer['message']['id'];
+                $user->save();
+            }
+        }else {
+            // TODO @todo Testing only , remove after set production 
+            $user->tap_customer_id = "cus_TS03A3920231337Jw212412549";
+            $user->save();  
+        }
     }
 }
