aa2571b28b7ab897509a9612d1054842dc9e3a90
Khaled Alam <khaledalam.net@gmail.com>
2024-01-06 22:54:18 +0800

    fix route name.

--- backend/resources/views/restaurant/components/payment-tap-documents-alert.blade.php
+++ backend/resources/views/restaurant/components/payment-tap-documents-alert.blade.php
@@ -1,5 +1,5 @@
 @if (!\App\Models\Tenant\Tap\TapBusiness::first())
     <div class="alert alert-warning">
-        <span class="badge badge-danger mx-1">&#9432;</span> {{__('messages.payment-gateway-approve-warning-text')}} <a href="{{route('payment-gateway.payments_upload_tap_documents_get')}}"><u>{{__('messages.payment-gateway-approve-warning-link-text')}}</u></a>.
+        <span class="badge badge-danger mx-1">&#9432;</span> {{__('messages.payment-gateway-approve-warning-text')}} <a href="{{route('tap.payments_upload_tap_documents_get')}}"><u>{{__('messages.payment-gateway-approve-warning-link-text')}}</u></a>.
     </div>
 @endif
