207dd2aef8b5afa5b83b2f832147257817d51b93
Precioussoul <habsoff@gmail.com>
2024-01-07 18:57:02 +0100

    feat: finalized on cardPayment option menu

--- backend/resources/tenant/src/pages/CustomerPage/components/CardPayment.jsx
+++ backend/resources/tenant/src/pages/CustomerPage/components/CardPayment.jsx
@@ -1,30 +1,39 @@
 import React from "react"
 import imgCardSim from "../../../assets/cardSim.svg"
-import imgVisa from "../../../assets/visa.svg"
-import imgMasterCard from "../../../assets/mastercard.svg"
 import {MdMoreVert} from "react-icons/md"
 
-const CardPayment = () => {
+const CardPayment = ({poweredByImgURl, CardNumber, ValidThruNo}) => {
   return (
-    <div className='relative p-4 h-[270px] w-[470px] flex items-center justify-center border border-[var(--customer)] rounded-xl'>
-      <div onClick={() => {}} className='absolute top-2 right-2 cursor-pointer'>
-        <MdMoreVert size={26} color={"#E16449"} />
+    <div className='relative p-4 h-[270px] w-[430px] flex items-center justify-center border border-[var(--customer)] rounded-xl'>
+      <div className='dropdown absolute top-2 right-2'>
+        <div tabIndex={0} role='button' className=''>
+          <MdMoreVert size={26} color={"#E16449"} />
+        </div>
+        <ul
+          tabIndex={0}
+          className='dropdown-content z-[2] menu shadow bg-base-100 rounded-lg w-52 h-[54px]'
+        >
+          <li className='bg-[var(--customer)] text-white font-bold text-center w-full p-2 cursor-pointer'>
+            Delete
+          </li>
+        </ul>
       </div>
-      <div className='relative w-[400px] h-full text-white'>
+
+      <div className='relative w-[350px] h-full text-white'>
         <div
-          className='h-[200px] w-[400px] flex items-center justify-center absolute top-0 right-0 left-0 rounded-xl'
+          className='h-[200px] w-[350px] flex items-center justify-center absolute top-0 right-0 left-0 rounded-xl'
           style={{background: " rgba(225, 100, 73, 0.62)"}}
         >
           <div className='h-6 w-full bg-black'></div>
         </div>
         <div
-          className='h-[200px] w-[390px] absolute top-6 right-2 left-[-0.8rem] rounded-xl'
+          className='h-[200px] w-[350px] absolute top-6 right-2 left-[-0.8rem] rounded-xl'
           style={{
             background:
               "linear-gradient(125deg, rgba(225,100,73,1) 58%, rgba(242,36,35,1) 58%)",
           }}
         >
-          <div className='w-full h-full p-4 flex flex-col gap-5 text-white'>
+          <div className=' h-full p-4 flex flex-col gap-5 text-white'>
             <div className='w-full flex items-start justify-between'>
               <div className='flex flex-col gap-1'>
                 <h3 className=''>Fincard</h3>
@@ -33,11 +42,11 @@ const CardPayment = () => {
               <h3 className='font-bold text-sm'>{"Platinum Debit"}</h3>
             </div>
             <div className='w-full'>
-              <h3 className='font-bold text-xl'>5214 32** **** 1673</h3>
+              <h3 className='font-bold text-xl'>{CardNumber}</h3>
             </div>
             <div className='w-full flex items-center mt-2 justify-between'>
-              <h3 className=''>Valid Thru {"11/23"}</h3>
-              <img src={imgVisa} alt='' className='' />
+              <h3 className=''>Valid Thru {ValidThruNo}</h3>
+              <img src={poweredByImgURl} alt='' className='' />
             </div>
           </div>
         </div>
--- backend/resources/tenant/src/pages/CustomerPage/components/CustomerPayment.jsx
+++ backend/resources/tenant/src/pages/CustomerPage/components/CustomerPayment.jsx
@@ -1,6 +1,8 @@
 import React from "react"
 import CardPayment from "./CardPayment"
 import imgPayment from "../../../assets/cardProfileIcon.svg"
+import imgVisa from "../../../assets/visa.svg"
+import imgMasterCard from "../../../assets/mastercard.svg"
 
 const CustomerPayment = () => {
   return (
@@ -12,8 +14,35 @@ const CustomerPayment = () => {
       <h3 className='border-b inline-block border-[var(--customer)] text-xl pb-1 my-5'>
         My Card
       </h3>
-      <div className=''>
-        <CardPayment />
+      <div className='w-auto overflow-x-scroll h-full flex items-center gap-14 my-5'>
+        <div className='w-max'>
+          <CardPayment
+            poweredByImgURl={imgVisa}
+            CardNumber={"5214  32**  ****  1673"}
+            ValidThruNo={"11/27"}
+          />
+        </div>
+        <div className='w-max'>
+          <CardPayment
+            poweredByImgURl={imgMasterCard}
+            CardNumber={"5214  32**  ****  1673"}
+            ValidThruNo={"11/27"}
+          />
+        </div>
+        <div className='w-max'>
+          <CardPayment
+            poweredByImgURl={imgVisa}
+            CardNumber={"5214  32**  ****  1673"}
+            ValidThruNo={"11/27"}
+          />
+        </div>
+        <div className='w-max'>
+          <CardPayment
+            poweredByImgURl={imgMasterCard}
+            CardNumber={"5214  32**  ****  1673"}
+            ValidThruNo={"11/27"}
+          />
+        </div>
       </div>
     </div>
   )
