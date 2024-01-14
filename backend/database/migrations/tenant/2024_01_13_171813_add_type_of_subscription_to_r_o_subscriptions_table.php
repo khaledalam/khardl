dafac831868794e0ce8dc4b9f541ac6b261527b2
Khaled Alam <khaledalam.net@gmail.com>
2024-01-14 10:47:34 +0800

    feat: add the down migrations.

--- backend/database/migrations/tenant/2024_01_13_171813_add_type_of_subscription_to_r_o_subscriptions_table.php
+++ backend/database/migrations/tenant/2024_01_13_171813_add_type_of_subscription_to_r_o_subscriptions_table.php
@@ -22,7 +22,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('r_o_subscriptions', function (Blueprint $table) {
-            //
+            $table->dropColumn('type');
+
         });
     }
 };
