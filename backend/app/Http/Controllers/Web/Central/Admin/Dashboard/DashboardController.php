969b8843074b65a32aa6c1fcd78165548fb387bc
Khaled Alam <khaledalam.net@gmail.com>
2024-01-21 02:49:48 +0200

    style: Remove unused imports.

--- backend/app/Http/Controllers/Web/Central/Admin/Dashboard/DashboardController.php
+++ backend/app/Http/Controllers/Web/Central/Admin/Dashboard/DashboardController.php
@@ -4,20 +4,14 @@ namespace App\Http\Controllers\Web\Central\Admin\Dashboard;
 
 use App\Http\Controllers\Controller;
 use App\Http\Services\Central\Admin\Dashboard\DashboardService;
-use App\Jobs\SendTAPLeadIDMerchantIDRequestEmailJob;
-use App\Mail\TAPLeadIDToMerchantIDRequest;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
-
 {
     public function __construct(private DashboardService $dashboardService) {
     }
     public function index(Request $request)
     {
-//        dd("test");
-        SendTAPLeadIDMerchantIDRequestEmailJob::dispatch(Auth::user(), 'test');
         return $this->dashboardService->index($request);
     }
 }
