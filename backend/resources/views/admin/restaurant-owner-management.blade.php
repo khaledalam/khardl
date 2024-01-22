7be92be4793399f57627baebbf604750d2ecda5b
sezohessen <sezohessen2000@gmail.com>
2024-01-22 16:56:34 +0200

    🐛 FIX: Restaurant owner page contains a button to add new admin, which on click goes to the page /add-admin

--- backend/resources/views/admin/restaurant-owner-management.blade.php
+++ backend/resources/views/admin/restaurant-owner-management.blade.php
@@ -33,18 +33,6 @@
                             </select>
                         </div>
                     </form>
-
-                    <div class="card-toolbar" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-trigger="hover" title="{{ __('messages.Add') }}">
-                        <a href="{{ route('admin.add-user') }}" class="btn btn-sm btn-light btn-active-primary">
-                            <!--begin::Svg Icon | path: icons/duotune/arrows/arr075.svg-->
-                            <span class="svg-icon svg-icon-3">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                    <rect opacity="0.5" x="11.364" y="20.364" width="16" height="2" rx="1" transform="rotate(-90 11.364 20.364)" fill="currentColor" />
-                                    <rect x="4.36396" y="11.364" width="16" height="2" rx="1" fill="currentColor" />
-                                </svg>
-                            </span>
-                            <!--end::Svg Icon-->{{ __('messages.new-admin')}}</a>
-                    </div>
                 </div>
                 <!--end::Header-->
                 <!--begin::Body-->
