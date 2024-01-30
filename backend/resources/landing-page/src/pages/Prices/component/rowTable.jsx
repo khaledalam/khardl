cee8b1af270f1b63279e0d4bc57da7e26a59ab07
Shabbir <fronteddevops@gmail.com>
2024-01-30 16:56:55 +0530

    price card ui fixes

--- backend/resources/landing-page/src/pages/Prices/component/rowTable.jsx
+++ backend/resources/landing-page/src/pages/Prices/component/rowTable.jsx
@@ -5,12 +5,20 @@ const RowTable = ({ points, price, without }) => {
     const { t } = useTranslation();
 
     return (
+        <>
         <div className='w-[100%] content max-md:w-[100%]'>
-            <div className='grid grid-cols-2 text-center items-center bg-white py-3'>
-                <h1 className='content'>{points} {without ? "" : <>{t("point")}</> }</h1>
-                <h1> <span className="small">{t("SAR")}</span><span className='price'>{price}</span><span className="small">{t("SAR")}</span></h1>
+            <div className='text-center items-center bg-white py-3'>
+                {/* <h1 className='content'>{points} {without ? "" : <>{t("point")}</> }</h1> */}
+                <h1> <span className="small">{t("SAR")}</span><span className='price'>388</span><span className="small">/Branch</span><span className="text-[#C0D123] ms-2">* Yearly</span></h1>
             </div>
         </div>
+        <div className='w-[100%] content max-md:w-[100%]'>
+        <div className='text-center items-center bg-white py-3'>
+            {/* <h1 className='content'>{points} {without ? "" : <>{t("point")}</> }</h1> */}
+            <h1> <span className="small">{t("SAR")}</span><span className='price'>0,75</span><span className="small">/Order</span><span className="text-[#C0D123] ms-2">* Flat</span></h1>
+        </div>
+    </div>
+    </>
     )
 }
 
