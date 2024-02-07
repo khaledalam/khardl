e0ca35ba65566abef5c8c229062d2294997ca4c2
sezohessen <sezohessen2000@gmail.com>
2024-02-07 15:22:18 +0200

    👌 IMPROVE: Testing update branch

---
+++ backend/tests/Feature/API/Branch/UpdateBranchSettingsRequestTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature\API\Auth\Login\Branch;
+
+
+use App\Models\Tenant\Branch;
+use Tests\Feature\API\WorkerBase;
+
+
+class UpdateBranchSettingsRequestTest extends WorkerBase
+{
+    /* branches/{branch}/delivery */
+    public function test_update_delivery(): void
+    {
+        $branch = Branch::factory()->create();
+        $this->worker->branch_id = $branch->id;
+        $this->worker->save();
+        $path = $this->baseURL."api/branches/$branch->id/delivery";
+        $data = [
+            'delivery_availability' => fake()->boolean,
+            'pickup_availability' => fake()->boolean,
+            'preparation_time_delivery' => fake()->dateTime->format('H:i:s')
+        ];
+        $response = $this->putJson($path, $data, [
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json'
+        ]);
+        $response->dump();
+        $response->assertOk();
+    }
+
+
+}
