cfae54f15a9c7d7a61e1d5559835cd741b23faa8
Shabbir <fronteddevops@gmail.com>
2024-02-08 13:48:30 +0530

    stable

--- backend/resources/tenant/src/pages/RestuarantPage/components/Footer.jsx
+++ backend/resources/tenant/src/pages/RestuarantPage/components/Footer.jsx
@@ -1,15 +1,18 @@
-import React from "react"
-import imgWhatsapp from "../../../assets/whatsappImg.svg"
-import {useSelector} from "react-redux"
+import React from "react";
+import imgWhatsapp from "../../../assets/whatsappImg.svg";
+import { useSelector } from "react-redux";
+import {API_ENDPOINT} from "../../../config";
 
 const FooterRestuarant = () => {
-  const restaurantStyle = useSelector((state) => state.restuarantEditorStyle)
+  const language = useSelector((state) => state.languageMode.languageMode);
+  const restaurantStyle = useSelector((state) => state.restuarantEditorStyle);
+
 
   return (
-    <div className='w-full flex flex-col gap-4'>
+    <div className="w-full flex flex-col gap-4">
       <div
-        style={{background: restaurantStyle?.footer_color}}
-        className='w-full b h-[85px] flex flex-col gap-4  items-center justify-center'
+        style={{ background: restaurantStyle?.footer_color }}
+        className="w-full b h-[85px] flex flex-col gap-4  items-center justify-center"
       >
         <div
           className={`flex flex-col gap-3 ${
@@ -22,20 +25,20 @@ const FooterRestuarant = () => {
               : ""
           }`}
         >
-          <div className='flex items-center gap-3'>
+          <div className="flex items-center gap-3">
             {restaurantStyle?.selectedSocialIcons?.map((socialMedia, idx) => (
               <a
                 href={socialMedia?.link}
-                rel='noreferrer'
-                target='_blank'
-                className='cursor-pointer'
+                rel="noreferrer"
+                target="_blank"
+                className="cursor-pointer"
                 key={idx + "icon"}
               >
-                <div className='w-[30px] h-[30px]'>
+                <div className="w-[30px] h-[30px]">
                   <img
                     src={socialMedia?.imgUrl ? socialMedia.imgUrl : imgWhatsapp}
                     alt={socialMedia?.name ?? "social media"}
-                    className='w-full h-full object-contain'
+                    className="w-full h-full object-contain"
                   />
                 </div>
               </a>
@@ -57,36 +60,33 @@ const FooterRestuarant = () => {
             {" "}
             <a
               href={`tel:${restaurantStyle?.phoneNumber}`}
-              style={{color: restaurantStyle?.text_color}}
-              className='font-semibold cursor-pointer'
+              style={{ color: restaurantStyle?.text_color }}
+              className="font-semibold cursor-pointer"
             >
               {restaurantStyle?.phoneNumber}
             </a>
           </div>
         )}
       </div>
-      <div className='h-7 flex bg-white items-center justify-between p-2'>
-        <h3 className='pl-16 text-[1rem] text-neutral-700 font-medium'>
-          Powered by{" "}
-          <a
-            href='https://first.khardl.com'
-            className='text-neutral-400 text-sm cursor-pointer'
-          >
-            Khardl
-          </a>
-        </h3>
-        <h3 className='pl-16 text-[1rem] text-neutral-700 font-medium'>
-          مشغل بواسطة{" "}
-          <a
-            href='https://first.khardl.com'
-            className='text-neutral-400 text-sm cursor-pointer'
-          >
-            خردل
-          </a>
-        </h3>
+      <div className="h-7 flex bg-white items-center justify-between p-2">
+        {language === "en" ? (
+          <h3 className="pl-16 text-[1rem] text-neutral-700 font-medium">Powered by{" "}
+            <a
+            href={API_ENDPOINT}
+              className="text-neutral-400 text-sm cursor-pointer"
+            >Khardl</a>
+          </h3>
+        ) : (
+          <h3 className="pl-16 text-[1rem] text-neutral-700 font-medium">مشغل بواسطة{" "}
+            <a
+              href={API_ENDPOINT}
+              className="text-neutral-400 text-sm cursor-pointer"
+            >خردل</a>
+          </h3>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FooterRestuarant
+export default FooterRestuarant;
