ff038957a8c3df18605ba9ca4acbc44a16d194eb
Khaled Alam <khaledalam.net@gmail.com>
2024-02-08 10:00:20 +0200

    feat: inital empty deliveryType in cart radio buttons.

--- backend/resources/tenant/src/pages/CartPage/components/PaymentSection.jsx
+++ backend/resources/tenant/src/pages/CartPage/components/PaymentSection.jsx
@@ -37,7 +37,7 @@ const PaymentSection = ({
   const { t } = useTranslation()
   const [notes, setNotes] = useState("")
   const [couponCode, setCouponCode] = useState("")
-  const [deliveryType, setDeliveryType] = useState(deliveryTypes && deliveryTypes[0] ? deliveryTypes[0]?.name : "");
+  const [deliveryType, setDeliveryType] = useState( "");
   const [couponDiscountValue, setCouponDiscountValue] = useState(null)
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn)
   const [paymentMethod, setPaymentMethod] = useState(paymentMethods && paymentMethods[0] ? paymentMethods[0]?.name : "")
