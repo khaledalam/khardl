5e844af24169735b924627aad537d55ea922add8
Shabbir <fronteddevops@gmail.com>
2024-02-08 19:37:16 +0530

    stable

--- backend/resources/tenant/src/pages/EditorsPage/Restuarants/components/ProductItem.jsx
+++ backend/resources/tenant/src/pages/EditorsPage/Restuarants/components/ProductItem.jsx
@@ -81,6 +81,7 @@ const ProductItem = ({
       setQtyCount((prev) => prev - 1);
     }
   }, [qtyCount]);
+
   const branch_id = localStorage.getItem("selected_branch_id");
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   const categories = useSelector((state) => state.categoryAPI.categories);
@@ -283,6 +284,7 @@ const ProductItem = ({
 
   const handleAddToCart = async () => {
     try {
+      setSpinner(true);
       let payload = {
         item_id: id,
         quantity: qtyCount,
@@ -296,13 +298,14 @@ const ProductItem = ({
       const response = await AxiosInstance.post(`/carts`, payload);
 
       console.log("response ", response);
-
       if (response?.data) {
         toast.success(`${t("Item added to cart")}`);
         setGotoCart(true);
         fetchCartData();
+        setSpinner(false);
       }
     } catch (error) {
+      setSpinner(false);
       console.log(error);
 
       toast.error(error.response?.data?.message);
@@ -376,34 +379,6 @@ const ProductItem = ({
 
   return (
     <Fragment>
-        {spinner && (
-        <div
-          role="status"
-          className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-full z-50"
-        >
-          <div className="absolute inset-0 bg-black opacity-25"></div>
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            <svg
-              aria-hidden="true"
-              className="w-8 h-8 mr-2 text-gray-200 animate-spin fill-[var(--primary)]"
-              viewBox="0 0 100 101"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                fill="currentColor"
-              />
-              <path
-                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                fill="currentFill"
-              />
-            </svg>
-            <span className="sr-only">Loading...</span>
-          </div>
-        </div>
-      )}
-
       <div
         key={valuekey}
         style={{
@@ -514,6 +489,34 @@ const ProductItem = ({
           </form>
           {isLoggedIn ? (
             <Fragment>
+              {spinner && (
+                <div
+                  role="status"
+                  className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-full z-100"
+                >
+                  <div className="absolute inset-0 bg-black opacity-25"></div>
+                  <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                    <svg
+                      aria-hidden="true"
+                      className="w-8 h-8 mr-2 text-gray-200 animate-spin fill-[var(--primary)]"
+                      viewBox="0 0 100 101"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="currentFill"
+                      />
+                    </svg>
+                    <span className="sr-only">Loading...</span>
+                  </div>
+                </div>
+              )}
+
               <div
                 className={`bg-white w-full rounded-t-[80px]   ${
                   checkboxItems[0]?.length > 0 ||
@@ -715,9 +718,13 @@ const ProductItem = ({
                     <IoAddCircleOutline size={28} onClick={incrementQty} />
                   </div>
 
-                  <div onClick={() => navigate("/cart")}
-                   style={{backgroundColor: cartBgcolor ? cartBgcolor : "#F2FF00",}}
-                    className="w-[45px] h-[45px] relative flex items-center justify-center rounded-lg cursor-pointer">
+                  <div
+                    onClick={() => navigate("/cart")}
+                    style={{
+                      backgroundColor: cartBgcolor ? cartBgcolor : "#F2FF00",
+                    }}
+                    className="w-[45px] h-[45px] relative flex items-center justify-center rounded-lg cursor-pointer"
+                  >
                     <img src={cartHeaderImg} alt={"cart"} className="" />
                   </div>
 
@@ -736,20 +743,19 @@ const ProductItem = ({
                           className="w-full h-full object-contain "
                         />
                       </div>
-               
-                        <h3
-                          style={{
-                            color: amountColor
-                              ? amountColor
-                              : cartBgcolor
-                              ? "white"
-                              : "red",
-                          }}
-                          className="text-[14px] font-bold"
-                        >
-                          {t("SAR")} {totalPrice && finalPrice}
-                        </h3>
-                     
+
+                      <h3
+                        style={{
+                          color: amountColor
+                            ? amountColor
+                            : cartBgcolor
+                            ? "white"
+                            : "red",
+                        }}
+                        className="text-[14px] font-bold"
+                      >
+                        {t("SAR")} {totalPrice && finalPrice}
+                      </h3>
                     </div>
                   ) : (
                     <div
