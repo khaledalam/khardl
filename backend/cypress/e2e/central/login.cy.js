97eae1406b77dc9ffe8e4fa4aafc781f46736b7a
Khaled Alam <khaledalam.net@gmail.com>
2024-02-14 17:39:57 +0400

    cypress: Fix login error testcase.

--- backend/cypress/e2e/central/login.cy.js
+++ backend/cypress/e2e/central/login.cy.js
@@ -1,10 +1,18 @@
 import {url} from '../url';
 
 describe('Test Central Homepage | Auth | Login page', () => {
-    it('test login with invalid email', () => {
+    it('test login with empty fields', () => {
         cy.visit(url + '/login');
         // cy.contains('EN').click();
-        cy.get('input[type="email"]').type('test');
-        cy.get('button[type="submit"]').click();
+        cy.contains('تسجيل الدخول').get('button[type="submit"]').click();
+        cy.contains('يرجى إدخال البريد الالكتروني');
+        cy.get('input[type=email]').type('test@test.com');
+        cy.contains('يرجى إدخال كلمة المرور');
+        cy.get('input[type=password]').type('test123');
+        cy.contains('تسجيل الدخول').get('button[type="submit"]').click();
+        // @TODO: validate response
+        // cy.wait('@authenticate').its('response.statusCode').should('eq', 403)
+        // cy.wait('@authenticate').its('request.body').should('include', 'test@test.com')
+        // cy.wait('@authenticate').its('response.body').should('include', 'test123')
     });
 });
