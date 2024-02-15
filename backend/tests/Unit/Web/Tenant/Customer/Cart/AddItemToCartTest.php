7826be0a92545b78efa35f3888312bbbb981578f
sezohessen <sezohessen2000@gmail.com>
2024-02-15 20:20:08 +0200

    ✅ Adding Tests: Add item to cart V1 test

--- backend/tests/Unit/Web/Tenant/Customer/Cart/AddItemToCartTest.php
+++ backend/tests/Unit/Web/Tenant/Customer/Cart/AddItemToCartTest.php
@@ -26,16 +26,43 @@ class AddItemToCartTest extends TenantTestCase
         $this->actingAs($this->user,'web');
         $this->withoutMiddleware(RestaurantLive::class);
         $this->withoutMiddleware(RestaurantSubLive::class);
-        $item = Item::factory()->create();
-        dd($item);
     }
     public function createUser($options = null): RestaurantUser
     {
         return RestaurantUser::factory()->create($options);
     }
+    public function createItem($options = null): Item
+    {
+        return Item::factory()->create($options);
+    }
+    private function itemOption(Item $item,$required,$input_names)
+    {
+        $option = [];
+        if(!$item->$required)return null;
+        foreach ($item->$required as $key => $value) {
+            if($value=="true"){
+                $fakeIndexOption = fake()->numberBetween(0,count($item->$input_names[$key]) - 1);//Select one option
+                $option[$key] = $fakeIndexOption;
+            }
+        }
+        return $option;
+    }
     public function test_add_item_to_cart_success()
     {
-        $response = $this->postJson(self::path);
+        $item = $this->createItem();
+        $selectedCheckbox = $this->itemOption($item,'checkbox_required','checkbox_input_names');
+        $selectedRadio = $this->itemOption($item,'selection_required','selection_input_names');
+        $selectedDropdown = $this->itemOption($item,'dropdown_required','dropdown_input_names');
+        $response = $this->postJson(self::path,[
+            'item_id' => $item->id,
+            'quantity' => fake()->numberBetween(1,5),
+            'branch_id' => $this->user->branch->id,
+            'notes' => fake()->text,
+            'selectedCheckbox' => $selectedCheckbox,
+            'selectedRadio' => $selectedRadio,
+            'selectedDropdown' => $selectedDropdown,
+        ]);
         $response->dump();
+        $response->assertOk();
     }
 }
