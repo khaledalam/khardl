af18a05aa6d8ce7a6d381499712c035021373b23
Khaled Alam <khaledalam.net@gmail.com>
2024-02-19 03:03:04 +0400

    feat: allow origin.

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', env('APP_ENV') == 'local' ? '*' : null],
 
     'allowed_methods' => ['*'],
 
