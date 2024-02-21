d40144ec1a496111231dc2a5a43bd9a637d951ba
sezohessen <sezohessen2000@gmail.com>
2024-02-21 17:49:54 +0200

    ✅ Adding Tests: Testing promoter and promoters page

--- backend/tests/Feature/Web/Central/Promoter/PromoterPageTest.php
+++ backend/tests/Feature/Web/Central/Promoter/PromoterPageTest.php
@@ -42,7 +42,7 @@ class PromoterPageTest extends TestCase
             'attempts' => 1
         ]);
     }
-    public function test_many_failed_attempts()
+    public function test_promoter_page_many_failed_attempts()
     {
         $controller = new GlobalPromoterController();
         $this->insertPromoterFailedAttempt($controller::AttemptsCount);
@@ -62,6 +62,50 @@ class PromoterPageTest extends TestCase
             ]
         );
     }
-
-
+    public function test_attempts_increase_on_failed()
+    {
+        $count = 1;
+        $this->insertPromoterFailedAttempt($count);
+        $response = $this->getJson('/promoter/' . fake()->name);
+        $this->assertDatabaseHas('promoters_failed_attempts', [
+            'ip_address' => request()->ip(),
+            'attempts' => ++$count
+        ]);
+    }
+    public function test_promoters_page_success()
+    {
+        $response = $this->getJson('/promoters');
+        $response->assertDontSee(__('Not found promoter'));
+        $response->assertSee(__('Get information'));
+    }
+    public function test_promoters_page_not_found()
+    {
+        $name = fake()->name;
+        $response = $this->getJson('/promoters?name=' . $name);
+        $response->assertSee(__('Not found promoter'));
+        $response->assertSee($name);
+        $this->assertDatabaseHas('promoters_failed_attempts', [
+            'ip_address' => request()->ip(),
+            'attempts' => 1
+        ]);
+    }
+    public function test_promoters_search_success()
+    {
+        $promoter = $this->createPromoter();
+        $response = $this->getJson('/promoters?name=' . $promoter->name);
+        $response->assertDontSee(__('Not found promoter'));
+        $response->assertSee(__('You can view information of :name by click this',['name' => $promoter->name]));
+        $this->assertDatabaseMissing('promoters_failed_attempts', [
+            'ip_address' => request()->ip(),
+            'attempts' => 1
+        ]);
+    }
+    public function test_promoters_page_many_failed_attempts()
+    {
+        $controller = new GlobalPromoterController();
+        $this->insertPromoterFailedAttempt($controller::AttemptsCount);
+        $response = $this->getJson('/promoters?name=' . fake()->name);
+        $response->assertRedirectToRoute('home');
+        $response->assertDontSee(__('Not found promoter'));
+    }
 }
