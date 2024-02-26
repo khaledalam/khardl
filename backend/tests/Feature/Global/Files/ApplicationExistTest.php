50eb4e711e167ab46c5456286c52058eb50a759e
Khaled Alam <khaledalam.net@gmail.com>
2024-02-26 11:38:10 +0400

    ct/cd: debug app url.

--- backend/tests/Feature/Global/Files/ApplicationExistTest.php
+++ backend/tests/Feature/Global/Files/ApplicationExistTest.php
@@ -11,6 +11,9 @@ class ApplicationExistTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
+
+        echo 'APP_URL: ' . env('APP_URL');
+
         $response = $this->get(env('APP_URL'));
         $response->assertStatus(200);
     }
