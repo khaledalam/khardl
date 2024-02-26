26b2e274694f9aaa04ef8af67e36422303fabe64
Khaled Alam <khaledalam.net@gmail.com>
2024-02-26 15:09:36 +0400

    feat: add logic of send contact us form inputs to admin email.

---
+++ backend/app/Mail/ContactUsMail.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+/**
+ * This is used to send Lead ID to TAP so that they provide use with Merchant ID for now,
+ * till their API be public
+ */
+class ContactUsMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public string $email;
+    public string $phone_number;
+    public string $business_name;
+    public string $responsible_person_name;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param string $lead_id
+     * @return void
+     */
+    public function __construct(
+        string $email,
+        string $phone_number,
+        string $business_name,
+        string $responsible_person_name
+    )
+    {
+        $this->email = $email;
+        $this->phone_number = $phone_number;
+        $this->business_name = $business_name;
+        $this->responsible_person_name = $responsible_person_name;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->to([
+//               env('SUPER_MASTER_ADMIN_EMAIL')
+             'khaledalam.net@gmail.com' // @TODO: remove this line
+            ])
+            ->cc([
+                'khaledalam.net@gmail.com',
+                'aldahman.ibrahim@gmail.com',
+            ])
+            ->subject('Khardl | Contact Us request')
+            ->view('emails.tap_leadid_merchantid_email');
+    }
+}
