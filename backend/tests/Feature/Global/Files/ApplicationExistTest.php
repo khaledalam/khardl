ec477429479a897dab58f80c8e9c130a9e5d138f
Khaled Alam <khaledalam.net@gmail.com>
2024-02-28 13:07:28 +0400

    ci/cd: debuging run app in pipelines.

--- backend/tests/Feature/Global/Files/ApplicationExistTest.php
+++ backend/tests/Feature/Global/Files/ApplicationExistTest.php
@@ -11,12 +11,6 @@ class ApplicationExistTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-
-        echo 'APP_URL: ' . env('APP_URL');
-
-        exec('curl -I http://khardl:8000');
-        exec('curl -I http://127.0.0.1:8000');
-
         $response = $this->get(env('APP_URL'));
         $response->assertStatus(200);
     }
