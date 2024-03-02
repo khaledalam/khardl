8338ed89b7302ee93715b251eeedbcd19e2ffac7
Omar Taha <o.taha@dashcode.co>
2024-03-02 21:14:16 +0200

    fix landing page btns

--- backend/resources/landing-page/src/components/ButtonComponent.jsx
+++ backend/resources/landing-page/src/components/ButtonComponent.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-const Button = ({ title, classContainer, onClick, icon, link }) => {
-  console.log(classContainer, icon, "icon");
+const Button = ({ title, classContainer, onClick, icon, link, className }) => {
+    console.log(classContainer, icon, "icon");
 
-  return (
-    <Link to={link}>
-      <button onClick={onClick} className={``}>
-        {title} {icon ? <icon /> : null}
-      </button>
-    </Link>
-  );
+    return (
+        <Link className={className} to={link}>
+            <button onClick={onClick}>
+                {title} {icon ? <icon /> : null}
+            </button>
+        </Link>
+    );
 };
 
 export default Button;
