4b6e5f3efc2d932bbbd1366d84bb5dda39d5668f
sezohessen <sezohessen2000@gmail.com>
2024-03-12 06:09:42 +0200

    👌 IMPROVE: update customer data

--- backend/app/Http/Controllers/Web/Tenant/Customer/CustomerDataController.php
+++ backend/app/Http/Controllers/Web/Tenant/Customer/CustomerDataController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Web\Tenant\Customer;
 
 use App\Http\Controllers\Web\BaseController;
 use App\Http\Requests\Web\Tenant\Customer\ChangeUserStatusFormRequest;
+use App\Http\Requests\Web\Tenant\Customer\UpdateCustomerFormRequest;
 use App\Http\Services\tenant\Customer\CustomerDataService;
 use App\Models\Tenant\RestaurantUser;
 use Illuminate\Support\Facades\Auth;
@@ -23,6 +24,14 @@ class CustomerDataController extends BaseController
     public function show(Request $request,RestaurantUser $restaurantUser)
     {
        return $this->customerDataService->show($request,$restaurantUser);
+    }
+    public function edit(Request $request,RestaurantUser $restaurantUser)
+    {
+       return $this->customerDataService->edit($request,$restaurantUser);
+    }
+    public function update(UpdateCustomerFormRequest $request,RestaurantUser $restaurantUser)
+    {
+       return $this->customerDataService->update($request,$restaurantUser);
     }
     public function update_status(ChangeUserStatusFormRequest $request, RestaurantUser $restaurantUser)
     {
