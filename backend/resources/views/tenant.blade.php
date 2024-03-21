a5165ee40c116f99b92fbc6b75a3c60bf5ecb352
Khaled Alam <khaledalam.net@gmail.com>
2024-03-21 14:05:09 +0400

    feat: R- title only.

--- backend/resources/views/tenant.blade.php
+++ backend/resources/views/tenant.blade.php
@@ -11,7 +11,7 @@
     <meta property="og:description" content="Khardl, Digital Ecosystem Solution For Restaurants, Create your website and app with Khardl in minutes, start selling right away, and pay based on your orders only" />
     <meta property="og:image" content="{{ $logo ?? global_asset('img/logo.png') }}" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ucfirst($restaurant_name)}} - {{__('khardl')}}</title>
+    <title>{{ucfirst($restaurant_name)}}</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') . "?id=" . trim(exec('git log --pretty="%h" -n1 HEAD')) }}">
     <link rel="canonical" href="{{ $logo ?? global_asset('img/logo.png') }}" />
     <link rel="shortcut png" href="{{ $logo ?? global_asset('img/logo.png') }}"/>
