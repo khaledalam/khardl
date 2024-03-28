ebd7e3226ace8ed3aca754af88833d82532cc9d7
Khaled Alam <khaledalam.net@gmail.com>
2024-03-28 21:43:46 +0400

    feat: fix migration.

--- backend/database/migrations/2024_03_25_155045_add_dob_and_restaurant_name_ar_to_users_table.php
+++ backend/database/migrations/2024_03_25_155045_add_dob_and_restaurant_name_ar_to_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->date('dob');
+            $table->date('dob')->nullable();
             $table->string('restaurant_name_ar')->nullable();
         });
     }
