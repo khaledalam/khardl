7e340c490d9da03e3e7e9a68b055c02a4f23b531
Khaled Alam <khaledalam.net@gmail.com>
2024-04-05 02:14:41 +0400

    feat: replace actions dropdown to single button.

--- backend/resources/views/admin/restaraunts.blade.php
+++ backend/resources/views/admin/restaraunts.blade.php
@@ -186,62 +186,9 @@
                                         <!--end::Stats-->
                                         <!--begin::Action-->
                                         <div class="border border-gray-300 border-dashed rounded min-w-80px py-3 px-4 mb-3">
-                                            <a href="#" class="btn btn-sm btn-light btn-active-light-primary" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">{{ __('actions')}}
-                                                <!--begin::Svg Icon | path: icons/duotune/arrows/arr072.svg-->
-                                                <span class="svg-icon svg-icon-5 m-0">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                                        <path d="M11.4343 12.7344L7.25 8.55005C6.83579 8.13583 6.16421 8.13584 5.75 8.55005C5.33579 8.96426 5.33579 9.63583 5.75 10.05L11.2929 15.5929C11.6834 15.9835 12.3166 15.9835 12.7071 15.5929L18.25 10.05C18.6642 9.63584 18.6642 8.96426 18.25 8.55005C17.8358 8.13584 17.1642 8.13584 16.75 8.55005L12.5657 12.7344C12.2533 13.0468 11.7467 13.0468 11.4343 12.7344Z" fill="currentColor" />
-                                                    </svg>
-                                                </span>
-                                                <!--end::Svg Icon--></a>
-                                            <!--begin::Menu-->
-                                            <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-125px py-4" data-kt-menu="true">
-                                                <!--begin::Menu item-->
-                                                <div class="menu-item px-3">
-                                                    <a href="{{ route('admin.view-restaurants', ['tenant' => $restaurant->id]) }}" class="menu-link px-3">{{ __('view')}}</a>
-                                                </div>
-                                                <!--end::Menu item-->
-                                                <!--begin::Menu item-->
-                                                {{-- <div class="menu-item px-3">
-                                        <form id="delete-form{{ $restaurant->id }}" class="delete-form" action="{{ route('admin.delete-restaurant', ['id' => $restaurant->id]) }}" method="POST">
-                                                @method('DELETE')
-                                                @csrf
-                                                </form>
-                                                <a href="#" class="menu-link px-3" onclick="showDeleteConfirmation('{{ $restaurant->id }}')" data-kt-ecommerce-product-filter="delete_row">{{ __('delete')}}</a>
-                                            </div> --}}
-                                            <script>
-                                                var translations = @json([
-                                                    'cancelIt' => __('cancel')
-                                                    , 'yesDeleteIt' => __('yes-delete-it'),
-
-                                                ]);
-                                                var translationss = @json([
-                                                    'confirmDelete' => __('confirm-delete')
-                                                    , 'areYouSure' => __('are-you-sure-you-want-to-delete-this-restaurant'),
-
-                                                ]);
-
-
-
-                                                function showDeleteConfirmation(restaurantId) {
-                                                    Swal.fire({
-                                                        title: translationss.confirmDelete
-                                                        , text: translationss.areYouSure
-                                                        , icon: 'warning'
-                                                        , showCancelButton: true
-                                                        , confirmButtonText: translations.yesDeleteIt
-                                                        , cancelButtonText: translations.cancelIt
-                                                    }).then((result) => {
-                                                        if (result.isConfirmed) {
-                                                            document.getElementById('delete-form' + restaurantId).submit();
-                                                        }
-                                                    });
-                                                }
-
-                                            </script>
-                                            <!--end::Menu item-->
-                                        </div>
-                                        <!--end::Menu-->
+                                            <div class="menu-item px-3">
+                                                <a href="{{ route('admin.view-restaurants', ['tenant' => $restaurant->id]) }}" class="menu-link px-3">{{ __('view')}}</a>
+                                            </div>
                                     </div>
                                     <!--end::Action-->
                                 </div>
