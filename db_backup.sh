6815488a3983ae34dfaffea64b6720548f5a5557
Khaled Alam <khaledalam.net@gmail.com>
2024-04-07 01:55:37 +0400

    DB backup improve.

--- db_backup.sh
+++ db_backup.sh
@@ -8,7 +8,7 @@ now=$(date +"%Y_%m_%d_%H:%M:%S")
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR && /usr/bin/mysqldump $DATABASE_NAME  \
   > $DIR/db_backups/backup_$now.sql -u $DATABASE_USER --password=$DATABASE_PASSWORD \
-  --skip-extended-insert;
+  --skip-extended-insert --no-tablespaces;
 
 # Remove old backups (modified files more than 30 days ago)
 find $DIR/db_backups/* -mtime +30 -exec rm {} \;
