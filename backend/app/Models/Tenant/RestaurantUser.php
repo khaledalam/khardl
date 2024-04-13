030aca87e31301e0fc951a524d6c5316cc751ce0
sezohessen <sezohessen2000@gmail.com>
2024-04-13 17:01:22 +0200

    👌 IMPROVE: search by vehicle number

--- backend/app/Models/Tenant/RestaurantUser.php
+++ backend/app/Models/Tenant/RestaurantUser.php
@@ -154,6 +154,7 @@ class RestaurantUser extends Authenticatable implements MustVerifyEmail
             return $q->where('first_name', 'like', '%' . $search . '%')
             ->orWhere('last_name', 'like', '%' . $search . '%')
             ->orWhere('phone', 'like', '%' . $search . '%')
+            ->orWhere('vehicle_number', 'like', '%' . $search . '%')
             ->orWhere('email', 'like', '%' . $search . '%');
         });
     }
