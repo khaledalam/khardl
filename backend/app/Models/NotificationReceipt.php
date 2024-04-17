aa90a9e2fb0f78ab25280b6e7c2e3c36a7475b28
sezohessen <sezohessen2000@gmail.com>
2024-04-17 18:34:08 +0200

    💥 NEW: add notification receipt table and model

---
+++ backend/app/Models/NotificationReceipt.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class NotificationReceipt extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'email',
+        'name',
+        'active',
+        'is_branch_purchase',
+        'is_application_purchase',
+    ];
+    /* Scopes */
+    public function scopeWhenSearch($query,$search)
+    {
+        return $query->when($search != null, function ($q) use ($search) {
+            return $q->where('name', 'like', '%' . $search . '%')
+            ->orWhere('email', 'like', '%' . $search . '%');
+        });
+    }
+    public function scopeWhenStatus($query,$status)
+    {
+        return $query->when($status != null, function ($q) use ($status) {
+            return $q->where('active', (bool)$status);
+        });
+    }
+}
