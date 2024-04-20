bfebebbf90c6f8566973d075bb5063487e246d98
Karim Razgallah - Full Stack Developer <62987748+KarimRazgallah@users.noreply.github.com>
2024-04-20 11:26:16 +0100

    fix: banner slider aspect ratio

--- backend/resources/tenant/src/pages/EditorsPage/Restuarants/components/MainBoardEditor.jsx
+++ backend/resources/tenant/src/pages/EditorsPage/Restuarants/components/MainBoardEditor.jsx
@@ -503,7 +503,7 @@ const MainBoardEditor = ({
                 listofBannerImages?.length > 1 ? (
                     <>
                         <div
-                            className={`w-full h-[304px] ${
+                            className={`w-full aspect-[2/1] ${
                                 navItems[activeSection]?.title === "Banner" &&
                                 "shadow-inner border-[#C0D123] border-[2px] rounded-[10px]"
                             }`}
