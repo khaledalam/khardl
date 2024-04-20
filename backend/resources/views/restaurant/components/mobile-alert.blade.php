59eeac975d5c68e7479ed42e007c46dc7e7d4d79
sezohessen <sezohessen2000@gmail.com>
2024-04-20 18:46:33 +0200

    👌 IMPROVE: add a message when the user accesses the dashboard using mobile

---
+++ backend/resources/views/restaurant/components/mobile-alert.blade.php
@@ -0,0 +1,4 @@
+<div class="alert alert-warning mobile-alert" style="display: none">
+    <span class="badge badge-danger mx-1">&#9432;</span>
+    {{__('To get a better experience please use a computer')}}
+</div>
