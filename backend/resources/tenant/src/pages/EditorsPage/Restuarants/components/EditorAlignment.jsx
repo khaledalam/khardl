b2efd4f029c327245009ff8809e87403337c4d45
Khaled Alam <khaledalam.net@gmail.com>
2024-04-25 16:50:12 +0400

    site editor social media & pointer.

--- backend/resources/tenant/src/pages/EditorsPage/Restuarants/components/EditorAlignment.jsx
+++ backend/resources/tenant/src/pages/EditorsPage/Restuarants/components/EditorAlignment.jsx
@@ -37,7 +37,7 @@ const EditorAlignment = ({ defaultValue, onChange, modalId }) => {
             >
                 {alignments.map((alignment, idx) => (
                     <div
-                        className={`${
+                        className={`cursor-pointer ${
                             alignment.position === activeAlign
                                 ? "bg-white rounded-[50px] py-[10px] px-[25px]"
                                 : "rounded-[50px] py-[10px] px-[25px]"
