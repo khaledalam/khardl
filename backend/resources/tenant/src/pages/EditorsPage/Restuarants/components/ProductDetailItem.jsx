4cc4b818eb10654ef95dc86faf56c28d7b420147
Khaled Alam <khaledalam.net@gmail.com>
2024-04-25 15:32:33 +0400

    fix frontend.

--- backend/resources/tenant/src/pages/EditorsPage/Restuarants/components/ProductDetailItem.jsx
+++ backend/resources/tenant/src/pages/EditorsPage/Restuarants/components/ProductDetailItem.jsx
@@ -91,15 +91,20 @@ const ProductDetailItem = forwardRef(function ProductDetailItem(
                     <option disabled value="">
                         {t("select option")}
                     </option>
-                    {options.map((option, idx) => (
-                        <option key={idx} value={idx}>
-                            {`${
-                                language === "en"
-                                    ? option.value[0]
-                                    : option.value[1]
-                            } (${optionsPrice[idx]} ${t("SAR")})`}
-                        </option>
-                    ))}
+                    {options.map((option, idx) => {
+
+                        console.log(option);
+
+                        return (
+                            <option key={idx} value={idx}>
+                                {`${
+                                    language === "en"
+                                        ? option?.value[0]
+                                        : option?.value[1]
+                                } (${optionsPrice[idx]} ${t("SAR")})`}
+                            </option>
+                        )
+                    })}
                 </select>
             )}
         </Fragment>
