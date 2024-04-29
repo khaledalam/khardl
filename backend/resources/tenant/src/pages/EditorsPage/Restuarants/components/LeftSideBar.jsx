8b8f7f9f7bb3fbfb25da4eaa8fbba7df3a6116a5
Vitaly Sivkov <vitaly.sivkov@gmail.com>
2024-04-29 17:43:46 +0200

    refactor

--- backend/resources/tenant/src/pages/EditorsPage/Restuarants/components/LeftSideBar.jsx
+++ backend/resources/tenant/src/pages/EditorsPage/Restuarants/components/LeftSideBar.jsx
@@ -1,9 +1,9 @@
 import { useTranslation } from "react-i18next";
 import React, { useState } from "react";
-import FlashDown from "../../../../assets/flashDown.svg";
-import { use } from "i18next";
 import useWindowSize from "../../../../hooks/useWindowSize";
+import FlashDown from "../../../../assets/flashDown.svg";
 import GreenDot from "../../../../assets/greenDot.png";
+import { cn } from "../../../../utils/styles";
 
 export const LeftSideBar = ({
   activeSection,
@@ -15,7 +15,6 @@ export const LeftSideBar = ({
   setActiveDesignSection,
 }) => {
   const { t } = useTranslation();
-
   const toggleSection = (e, sectionIndex) => {
     if (activeSection === sectionIndex) {
       setActiveSection(null);
@@ -26,11 +25,7 @@ export const LeftSideBar = ({
     }
 
     setActiveDesignSection(null);
-    setMouseClick(e.clientX);
   };
-
-  const [mouseClick, setMouseClick] = useState(0);
-  const { width } = useWindowSize();
 
   return (
     <div className="flex flex-row h-[38px] md:h-full md:flex-col pl-[16px] md:px-[16px] z-40">
@@ -48,113 +43,115 @@ export const LeftSideBar = ({
         }}
       >
         {navItems.map((item, index) => (
-          <li
+          <Section
             key={`section-${index}`}
-            className="text-[#1118278A] text-[12px]  xl:text-[16px] font-light leading-[16px] relative w-full"
-          >
-            <div
-              className={`${
-                activeSection === index ? "bg-[#F3F3F3] text-[#111827]" : ""
-              } cursor-pointer whitespace-nowrap md:whitespace-normal py-[4px] md:py-[8px] px-[8px] md:pl-[24px] md:pr-[16px] rounded-[50px] flex flex-row justify-between items-center md:flex-none space-x-[8px] md:space-x-0 w-full`}
-              onClick={(e) => {
-                toggleSection(e, index);
-                item.subItems.length === 1 && setActiveSubitem(0);
-              }}
-            >
-              <div>
-                {item.subItems.length === 1
-                  ? item.subItems[0].title
-                  : item.title}
-              </div>
-              <div className="md:hidden w-[10px] h-[10px]">
-                <img
-                  src={FlashDown}
-                  alt="flash-down"
-                  className="object-contain w-full h-full"
-                />
-              </div>
-              <img
-                src={GreenDot}
-                alt="green-dot"
-                className={`${
-                  activeSection === index
-                    ? "hidden md:block md:w-[9px] md:h-[9px] md:object-contain"
-                    : "hidden"
-                }`}
-              />
-            </div>
-            {item.subItems.length !== 1 && (
-              <>
-                {width < 768 ? (
-                  <ul
-                    style={{
-                      zIndex: 11,
-                      left: Math.max(0, Math.min(200, mouseClick)),
-                    }}
-                    className={`${
-                      activeSection === index
-                        ? "block left-0 w-[137px] bg-white rounded-md border border-black border-opacity-10 fixed p-[8px] space-y-[8px]"
-                        : "hidden"
-                    }`}
-                  >
-                    {item.subItems.map((subItem, i) => (
-                      <li
-                        key={`sub-section-${i}`}
-                        className={`${
-                          activeSection === index && activeSubitem === i
-                            ? "bg-zinc-100 text-[#111827] border"
-                            : ""
-                        } w-[115px] h-6 rounded-md cursor-pointer py-[5px] px-[8px] flex flex-row justify-between items-center`}
-                        onClick={() => setActiveSubitem(i)}
-                      >
-                        <span>{subItem.title}</span>
-                        <img
-                          src={GreenDot}
-                          alt="green-dot"
-                          className={`${
-                            activeSection === index && activeSubitem === i
-                              ? "w-[9px] h-[9px] object-contain"
-                              : "hidden"
-                          }`}
-                        />
-                      </li>
-                    ))}
-                  </ul>
-                ) : (
-                  <ul
-                    className={`px-4 text-[10px] xl:text-[14px] leading-[13px] space-y-[16px] font-medium overflow-hidden transition-all duration-300 ${
-                      activeSection === index ? "max-h-40 mt-[16px]" : "max-h-0"
-                    } `}
-                  >
-                    {item.subItems.map((subItem, i) => (
-                      <li
-                        key={`sub-section-${i}`}
-                        className={`${
-                          activeSection === index && activeSubitem === i
-                            ? "bg-[#F3F3F3] text-[#111827]"
-                            : ""
-                        } ml-[14px] pt-[6px] pb-[5px] pl-[24px] pr-[16px] rounded-[50px] cursor-pointer flex justify-between items-center`}
-                        onClick={() => setActiveSubitem(i)}
-                      >
-                        <span>{subItem.title}</span>
-                        <img
-                          src={GreenDot}
-                          alt="green-dot"
-                          className={`${
-                            activeSection === index && activeSubitem === i
-                              ? "w-[9px] h-[9px] object-contain"
-                              : "hidden"
-                          }`}
-                        />
-                      </li>
-                    ))}
-                  </ul>
-                )}
-              </>
-            )}
-          </li>
+            title={item.title}
+            active={activeSection === index}
+            activeSubitem={activeSubitem}
+            subItems={item.subItems}
+            onClick={(e) => toggleSection(e, index)}
+          />
         ))}
       </ul>
     </div>
+  );
+};
+
+const Section = ({ title, subItems, active, activeSubitem, onClick }) => {
+  const [mouseClick, setMouseClick] = useState(0);
+  const { width } = useWindowSize();
+  const isMobile = width < 768;
+
+  const listDesktopClassName = cn(
+    "px-4 text-[10px] xl:text-[14px] leading-[13px] space-y-[16px] font-medium overflow-hidden transition-all duration-300",
+    active ? "max-h-40 mt-[16px]" : "max-h-0",
+  );
+
+  const listMobileClassName = active
+    ? "block left-0 w-[137px] bg-white rounded-md border border-black border-opacity-10 fixed p-[8px] space-y-[8px]"
+    : "hidden";
+
+  const handleSectionClick = (e) => {
+    setMouseClick(e.clientX);
+    onClick();
+  };
+
+  return (
+    <li className="text-[#1118278A] text-[12px]  xl:text-[16px] font-light leading-[16px] relative w-full">
+      <div
+        className={cn(
+          "cursor-pointer whitespace-nowrap md:whitespace-normal py-[4px] md:py-[8px] px-[8px] md:pl-[24px] md:pr-[16px] rounded-[50px] flex flex-row justify-between items-center md:flex-none space-x-[8px] md:space-x-0 w-full",
+          { "bg-[#F3F3F3] text-[#111827]": active },
+        )}
+        onClick={handleSectionClick}
+      >
+        <div>{title}</div>
+        <div className="md:hidden w-[10px] h-[10px]">
+          <img
+            src={FlashDown}
+            alt="flash-down"
+            className="object-contain w-full h-full"
+          />
+        </div>
+        <img
+          src={GreenDot}
+          alt="green-dot"
+          className={
+            active
+              ? "hidden md:block md:w-[9px] md:h-[9px] md:object-contain"
+              : "hidden"
+          }
+        />
+      </div>
+
+      <ul
+        className={cn(isMobile ? listMobileClassName : listDesktopClassName)}
+        style={
+          !isMobile
+            ? undefined
+            : {
+                zIndex: 11,
+                left: Math.max(0, Math.min(200, mouseClick)),
+              }
+        }
+      >
+        {subItems.map((subItem, i) => (
+          <SubItem
+            key={`sub-section-${i}`}
+            title={subItem.title}
+            active={active && activeSubitem === i}
+            onClick={() => setActiveSubitem(i)}
+          />
+        ))}
+      </ul>
+    </li>
+  );
+};
+
+const SubItem = ({ title, active, onClick }) => {
+  const { width } = useWindowSize();
+  const isMobile = width < 768;
+
+  const desktopClassName = cn(
+    "ml-[14px] pt-[6px] pb-[5px] pl-[24px] pr-[16px] rounded-[50px] cursor-pointer flex justify-between items-center",
+    { "bg-[#F3F3F3] text-[#111827]": active },
+  );
+  const mobileClassName = cn(
+    "w-[115px] h-6 rounded-md cursor-pointer py-[5px] px-[8px] flex flex-row justify-between items-center",
+    { "bg-zinc-100 text-[#111827] border": active },
+  );
+
+  return (
+    <li
+      className={isMobile ? mobileClassName : desktopClassName}
+      onClick={onClick}
+    >
+      <span>{title}</span>
+      <img
+        src={GreenDot}
+        alt="green-dot"
+        className={active ? "w-[9px] h-[9px] object-contain" : "hidden"}
+      />
+    </li>
   );
 };
