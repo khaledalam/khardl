d7f9bab04f83d0fb1f3954dcbbd63f82ecd1c4f4
demi-repo <cdadev24@gmail.com>
2024-05-02 18:48:42 +0200

    fix/restaurant landing page
    Remove the cart icon when it is empty, and udpate the position of modal, update the slider to be autochanged.

--- backend/resources/tenant/src/pages/EditorsPage/Restuarants/components/Sliderr.jsx
+++ backend/resources/tenant/src/pages/EditorsPage/Restuarants/components/Sliderr.jsx
@@ -5,44 +5,30 @@ import LeftIcon from "../../../../assets/leftIcon.png";
 const Sliderr = ({ banner_images, setIsBannerModalOpened }) => {
   const [imageIndex, setImageIndex] = useState(0);
 
-  const showNextImage = () => {
-    setImageIndex((imageIndex + 1) % banner_images.length);
+  const imageChange = () => {
+    console.log("SLIDER", imageIndex);
+    setImageIndex(prevIndex => (prevIndex + 1) % banner_images.length);
   };
-
-  const showPreviousImage = () => {
-    setImageIndex(
-      (imageIndex - 1 + banner_images.length) % banner_images.length
-    );
-  };
+  useEffect(() => {
+    const intervalId = setInterval(imageChange, 3000);
+    
+  }, [])
 
   return (
     <div className="h-full rounded-[10px] relative flex flex-col justify-center">
       <div className="flex w-full h-full rounded-[10px] overflow-x-hidden">
         {banner_images.map((image, index) => (
           <img
-          key={index}
-          src={image.croppedImage}
-          alt="banner"
-          className="flex aspect-[2/1] w-full rounded-[10px] object-cover shrink-0 grow-0 transition-transform duration-500 ease-in-out"
-          style={{ transform: `translateX(-${imageIndex * 100}%)` }}
-          onClick={() => setIsBannerModalOpened(true)}
+            key={index}
+            src={image.croppedImage}
+            alt="banner"
+            className="flex aspect-[2/1] w-full rounded-[10px] object-cover shrink-0 grow-0 transition-transform duration-500 ease-in-out"
+            style={{ transform: `translateX(-${imageIndex * 100}%)` }}
+            onClick={() => setIsBannerModalOpened(true)}
           />
         ))}
       </div>
-
-      <div
-        className="w-8 h-8 bg-gray-300 rounded-full bg-opacity-50 flex justify-center items-center absolute left-[16px] cursor-pointer hover:shadow-md hover:bg-opacity-100 transition-all"
-        onClick={showPreviousImage}
-      >
-        <img src={LeftIcon} className=" scale-125" alt="left" />
-      </div>
-      <div
-        className="w-8 h-8 bg-gray-300 rounded-full bg-opacity-50 flex justify-center items-center absolute right-[16px] cursor-pointer hover:shadow-md hover:bg-opacity-100 transition-all"
-        onClick={showNextImage}
-      >
-        <img src={RightIcon} className=" scale-125" alt="right" />
-      </div>
-      <div className="absolute bottom-[8px] left-[50%] translate-x-[-50%] flex gap-[3px] bg-black/[0.1] rounded-[50px] px-[35px] py-[6px] scale-150 hover:bg-opacity-50 transition-all">
+      <div className="absolute bottom-[8px] left-[50%] translate-x-[-50%] flex gap-[3px] bg-opacity-0 rounded-[50px] px-[35px] py-[6px] scale-150 hover:bg-opacity-50 transition-all">
         {banner_images.map((_, index) => (
           <button onClick={() => setImageIndex(index)} key={index}>
             {index == imageIndex ? (
