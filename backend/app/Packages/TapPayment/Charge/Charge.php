c066261121e1391dd844dd3a77357d6d8746d738
sezohessen <sezohessen2000@gmail.com>
2024-05-05 13:44:06 +0300

    👌 IMPROVE: fix charge

--- backend/app/Packages/TapPayment/Charge/Charge.php
+++ backend/app/Packages/TapPayment/Charge/Charge.php
@@ -31,8 +31,8 @@ class Charge extends Tap implements ChargeInterface
                 'id'=>env('TAP_PLATFORM_ID')
             ],
             'post'=>[
-                /* 'url'=>route('webhook-client-tap-payment') */
-                'url'=> 'https://d449-156-207-90-149.ngrok-free.app/webhook-tap-actions'
+                'url'=>route('webhook-client-tap-payment')
+                /* 'url'=> 'https://d449-156-207-90-149.ngrok-free.app/webhook-tap-actions' */
             ],
             // "source"=>[
             //     "id"=>"src_all"
@@ -62,8 +62,8 @@ class Charge extends Tap implements ChargeInterface
                 'id'=>$token_id
             ],
             'post'=>[
-                /* 'url'=>route('webhook-client-tap-payment') */
-                'url'=> 'https://d449-156-207-90-149.ngrok-free.app/webhook-tap-actions'
+                'url'=>route('webhook-client-tap-payment')
+                /* 'url'=> 'https://d449-156-207-90-149.ngrok-free.app/webhook-tap-actions' */
             ],
             // "source"=>[
             //     "id"=>"src_all"
