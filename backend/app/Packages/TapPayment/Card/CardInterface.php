4aae6a8c77bd93232f2f638ce8760c5701cd4b61
HassanElshazlyEida <hassanaboeata@gmail.com>
2024-05-06 17:54:32 +0300

    💥 NEW: list & delete customer cards

--- backend/app/Packages/TapPayment/Card/CardInterface.php
+++ backend/app/Packages/TapPayment/Card/CardInterface.php
@@ -8,5 +8,6 @@ interface CardInterface
 {
     public static function create(CreateBusinessRequest $request):array;
     public static function retrieve(string $token_id):array;
+    public static function delete(string $customer_id,string $card_id) :array;
 
 }
