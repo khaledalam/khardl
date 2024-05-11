5e04fd2c3751d31b5c0cb7044291bfbad84fbbce
Khaled Alam <khaledalam.net@gmail.com>
2024-05-11 07:56:40 +0400

    feat: add loyalty columns to items table.

--- backend/resources/views/restaurant/menu-category.blade.php
+++ backend/resources/views/restaurant/menu-category.blade.php
@@ -825,14 +825,14 @@
                                                                 <span class="required">{{__('Allow buy with loyalty points?')}}</span>
                                                             </label>
                                                             <label class="d-flex align-items-center fs-6 fw-bold mb-2">
-                                                                <input type="checkbox" name="all_buy_with_loyalty_points" checked value="1">
+                                                                <input type="checkbox" name="allow_buy_with_loyalty_points" checked value="1">
                                                             </label>
                                                         </div>
                                                         <!--end::Col-->
                                                         <!--begin::Col-->
                                                         <div class="col-md-6 fv-row">
-                                                            <label class="required fs-6 fw-bold mb-2">{{ __('Price using loyalty points') }}</label>
-                                                            <input type="number" step="0.1" min="1" required name="price_using_loyalty_points" value="{{ old('price_using_loyalty_points') ?? $item->price_using_loyalty_points }}" class="form-control form-control-solid ps-12" />
+                                                            <label class="fs-6 fw-bold mb-2">{{ __('Price using loyalty points (how many points)') }}</label>
+                                                            <input type="number" step="0.1" min="0.1" required name="price_using_loyalty_points" value="{{ old('price_using_loyalty_points') ?? $item->price_using_loyalty_points }}" class="form-control form-control-solid ps-12" />
                                                         </div>
                                                         <!--end::Col-->
 
