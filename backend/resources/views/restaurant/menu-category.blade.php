ef40507c529a0274712d9a40a552efe36c329b6c
Khaled Alam <khaledalam.net@gmail.com>
2024-05-12 08:33:52 +0400

    fix: item in popup.

--- backend/resources/views/restaurant/menu-category.blade.php
+++ backend/resources/views/restaurant/menu-category.blade.php
@@ -1179,14 +1179,14 @@
                                 <span class="required">{{__('Allow buy with loyalty points?')}}</span>
                             </label>
                             <label class="d-flex align-items-center fs-6 fw-bold mb-2">
-                                <input type="checkbox" id="allow_buy_with_loyalty_points-new" name="allow_buy_with_loyalty_points" @if($item?->price_using_loyalty_points) checked @endif value="1">
+                                <input type="checkbox" id="allow_buy_with_loyalty_points-new" name="allow_buy_with_loyalty_points" checked value="1">
                             </label>
                         </div>
                         <!--end::Col-->
                         <!--begin::Col-->
-                        <div class="col-md-6 fv-row @if(old('price_using_loyalty_points') ?? $item?->price_using_loyalty_points) d-block @else d-none @endif" id="loyalty_point_price_section-new">
+                        <div class="col-md-6 fv-row d-block" id="loyalty_point_price_section-new">
                             <label class="fs-6 fw-bold mb-2">{{ __('Price using loyalty points (how many points)') }}</label>
-                            <input type="number" step="0.1" min="0" required name="price_using_loyalty_points" value="{{ old('price_using_loyalty_points') ?? $item?->price_using_loyalty_points }}" class="form-control form-control-solid ps-12" />
+                            <input type="number" step="0.1" min="0" required name="price_using_loyalty_points" class="form-control form-control-solid ps-12" />
                         </div>
                         <!--end::Col-->
 
