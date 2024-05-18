0587e64def02ed603de7333b50978e76442c3085
Khaled Alam <khaledalam.net@gmail.com>
2024-05-18 18:52:25 +0400

    feat: remove paganation of address

--- backend/resources/views/restaurant/customers_data/list.blade.php
+++ backend/resources/views/restaurant/customers_data/list.blade.php
@@ -75,46 +75,12 @@
                                                     <td>{{ $country ?? __('N/A') }}</td>
                                                     <td>{{ $ordersCount ?? __('N/A') }}</td>
                                                     <td>{{ $rank++ }}</td>
-
                                                 </tr>
                                                 <!--end::Table row-->
                                             @endforeach
                                         @endforeach
                                     @endforeach
                                 </tbody>
-                                <!--end::Table head-->
-                                <form id="approve-form"  method="POST" style="display: inline">
-                                    @csrf
-                                    @method('PUT')
-                                    <input type="hidden" name="status" id="orderStatus" >
-                                </form>
-                                <script>
-                                    function showConfirmation(customerId) {
-                                        event.preventDefault();
-                                        const statusOptions = @json(array_combine(\App\Models\Tenant\RestaurantUser::STATUS,array_map(fn ($status) => __(''.$status), \App\Models\Tenant\RestaurantUser::STATUS)));
-
-                                        Swal.fire({
-                                            text: '{{ __('are-you-sure-you-want-to-change-order-status')}}',
-                                            icon: 'warning',
-                                            input: 'select',
-                                            showCancelButton: true,
-                                            inputOptions: statusOptions,
-                                            inputPlaceholder: 'Select an option',
-                                            confirmButtonText: `{{ __('yes') }}`,
-                                            cancelButtonText: `{{ __('no') }}`
-                                        }).then((result) => {
-                                            if (result.isConfirmed) {
-                                                const selectedStatus = result.value;
-                                                document.getElementById('orderStatus').setAttribute('value',selectedStatus);
-                                                var form = document.getElementById('approve-form');
-                                                form.action = `{{ route('customers_data.change-status', ['restaurantUser' => ':customerId']) }}`.replace(':customerId', customerId)
-                                                form.submit();
-
-                                            }
-                                        });
-                                    }
-                                </script>
-                                {{ $allCustomers->links('pagination::bootstrap-4') }}
                             </table>
                         </div>
                     </div>
