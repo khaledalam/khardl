3a67dfcb897aebf82b2eb17741929e2342e01656
sezohessen <sezohessen2000@gmail.com>
2024-05-19 12:05:14 +0300

    👌 IMPROVE: append driver info to the orders

--- backend/app/Http/Resources/API/Tenant/DriverResource.php
+++ backend/app/Http/Resources/API/Tenant/DriverResource.php
@@ -15,6 +15,7 @@ class DriverResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
             'email' => $this->email,
@@ -25,7 +26,7 @@ class DriverResource extends JsonResource
             'lat' => $this->lat,
             'lng' => $this->lng,
             'image' => $this->image,
-            'branch' => $this->branch,
+            /* 'branch' => $this->branch, */
         ];
     }
 }
