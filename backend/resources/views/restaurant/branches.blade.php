6ee21b7cf872a0be3d99c6ab6ead2a0a134eaae4
HassanElshazlyEida <hassanaboeata@gmail.com>
2024-05-26 08:11:52 +0300

    🐛 FIX: create new branch with map

--- backend/resources/views/restaurant/branches.blade.php
+++ backend/resources/views/restaurant/branches.blade.php
@@ -35,6 +35,7 @@ src="https://goSellJSLib.b-cdn.net/v2.0.0/js/gosell.js"
     } */
 
     #map-autocomplete-card-new_branch {
+        z-index: 100002 !important; 
         position: relative !important;
     }
     .map-overlay {
@@ -155,7 +156,7 @@ src="https://goSellJSLib.b-cdn.net/v2.0.0/js/gosell.js"
                             </div>
                             @if($available_branches > 0)
                             <div class="row gx-9 d-flex justify-content-center align-items-center">
-                                <a href="#" class="fs-6 text-700 fw-bolder text-center p-3 rounded fs-25" data-bs-toggle="modal" data-bs-target="#kt_modal_new_bransh" id="add-new-branch">+ {{ __('add-new-branch') }}</a>
+                                <a href="{{route('restaurant.create-branch')}}" class="fs-6 text-700 fw-bolder text-center p-3 rounded fs-25" >+ {{ __('add-new-branch') }}</a>
                             </div>
                             @endif
                         </div>
@@ -729,253 +730,6 @@ src="https://goSellJSLib.b-cdn.net/v2.0.0/js/gosell.js"
 
 
     </div>
-    <!--end::Content-->
-
-    <!--begin::Modal - New Target-->
-
-
-
-    <!--begin::Modal - New Branshes-->
-    <div class="modal fade" id="kt_modal_new_bransh" tabindex="-1" aria-hidden="true">
-        <!--begin::Modal dialog-->
-        <div class="modal-dialog modal-dialog-centered mw-650px">
-            <!--begin::Modal content-->
-            <div class="modal-content rounded">
-                <!--begin::Modal header-->
-                <div class="modal-header pb-0 border-0 justify-content-end">
-                    <!--begin::Close-->
-                    <div class="btn btn-sm btn-icon btn-active-color-khardl" data-bs-dismiss="modal">
-                        <!--begin::Svg Icon | path: icons/duotune/arrows/arr061.svg-->
-                        <span class="svg-icon svg-icon-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                fill="none">
-                                <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1"
-                                    transform="rotate(-45 6 17.3137)" fill="currentColor" />
-                                <rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)"
-                                    fill="currentColor" />
-                            </svg>
-                        </span>
-                        <!--end::Svg Icon-->
-                    </div>
-                    <!--end::Close-->
-                </div>
-                <!--begin::Modal header-->
-                <!--begin::Modal body-->
-                @if($available_branches > 0)
-                <div class="modal-body scroll-y pt-0 pb-15">
-                    <!--begin:Form-->
-                    <form id="kt_modal_new_bransh_form" class="form" action="{{ route('restaurant.add-branch') }}" method="POST" id="myForm">
-                        @csrf
-                        <!--begin::Heading-->
-                        <div class="mb-13 text-center d-flex justify-content-between align-items-center">
-                            <!--begin::Title-->
-                            <h1 class="mb-3">{{ __('add-new-branch') }}</h1>
-                            <!--end::Title-->
-                        </div>
-                        <!--end::Heading-->
-
-                        <!--begin::Input group-->
-                        <div class="row g-9 mb-8">
-                            <!--begin::Col-->
-                            <div class="col-md-12 fv-row">
-                                <label class="required fs-6 fw-bold mb-2">{{ __('name-branch') }}</label>
-                                <!--begin::Input-->
-                                <div class="position-relative d-flex align-items-center">
-                                    <!--begin::Datepicker-->
-                                    <input value="{{ old('name') }}" required name="name" class="form-control form-control-solid " type="text" />
-                                    <!--end::Datepicker-->
-                                </div>
-                                <!--end::Input-->
-                            </div>
-                            <!--end::Col-->
-                            <div class="col-md-12 fv-row">
-                                <label class="required fs-6 fw-bold mb-2">{{ __('phone') }}</label>
-                                <!--begin::Input-->
-                                <div class="position-relative d-flex align-items-center">
-                                    <!--begin::Datepicker-->
-                                    <input value="{{ old('phone') }}" required name="phone" class="form-control form-control-solid " type="text" />
-                                    <!--end::Datepicker-->
-                                </div>
-                                <!--end::Input-->
-                            </div>
-                            <!--end::Col-->
-                            <!--begin::Col-->
-
-                            <div class="col-md-12 fv-row" >
-                                <div id="map-autocomplete-card-new_branch" style="width: 80%;float: left;"></div>
-
-                                <label class="required fs-6 fw-bold mb-2">{{ __('location-branch') }}</label>
-{{--                                 <input id="pac-input-new_branch" class="form-control" type="hidden" placeholder="{{ __('search-for-place')}}" name="address">--}}
-                                <div style="width: 100%; height: 250px;" id="map-new_branch"></div>
-                                <input type="hidden" id="lat-new_branch" name="lat-new_branch" />
-                                <input type="hidden" id="lng-new_branch" name="lng-new_branch" />
-                                <!--end::Input-->
-                            </div>
-                            <!--end::Col-->
-                            <!--end::Col-->
-                            <div class="col-md-12 fv-row">
-                                <label class="fs-6 fw-bold mb-2">{{ __('city') }}</label>
-                                <!--begin::Input-->
-                                <div class="position-relative d-flex align-items-center">
-                                    <!--begin::Datepicker-->
-                                    <input value="{{ old('city') }}" required name="city" class="form-control form-control-solid " type="text" />
-                                    <!--end::Datepicker-->
-                                </div>
-                                <!--end::Input-->
-                            </div>
-                            <!--end::Col-->
-                            <!--end::Col-->
-                            <div class="col-md-12 fv-row">
-                                <label class="fs-6 fw-bold mb-2">{{ __('neighborhood') }}</label>
-                                <!--begin::Input-->
-                                <div class="position-relative d-flex align-items-center">
-                                    <!--begin::Datepicker-->
-                                    <input value="{{ old('neighborhood') }}" name="neighborhood" class="form-control form-control-solid " type="text" />
-                                    <!--end::Datepicker-->
-                                </div>
-                                <!--end::Input-->
-                            </div>
-                            <!--end::Col-->
-
-                            <!--begin::Col-->
-                            <div class="col-md-12 fv-row">
-                                <!--begin::Input-->
-                                <div class="position-relative d-flex align-items-center">
-                                <!--begin::Card body-->
-                                    <div class="card-body pt-0">
-                                        <!--begin::Select store template-->
-                                        <label for="kt_ecommerce_add_product_store_template" class="form-label required fs-6 fw-bold mb-2">{{ __('copy-menu') }}</label>
-                                        <!--end::Select store template-->
-                                        <!--begin::Select2-->
-                                        <select class="form-select mb-2" data-control="select2" data-hide-search="true" name="copy_menu" data-placeholder="{{ __('select-an-option') }}" id="kt_ecommerce_add_product_store_template">
-                                            <option value="None">{{ __('none') }}</option>
-                                            @foreach ($branches as $branch)
-                                                <option value="{{ $branch->id }}">{{ $branch->name }}</option>
-                                            @endforeach
-                                        </select>
-                                        <!--end::Select2-->
-                                    </div>
-                                    <!--end::Card body-->
-                                </div>
-                                <!--end::Input-->
-                            </div>
-                            <!--end::Col-->
-                            <div class="col-md-12 fv-row">
-                                <div class="position-relative d-flex align-items-center">
-                                    <div class="card-body pt-0">
-                                        <p class="form-label required fs-6 fw-bold mb-2">{{__("time")}}</p>
-                                        <div class="form-check form-check-inline">
-                                            <input class="form-check-input" type="radio" name="hours_option" id="normalChoice" value="normal" checked>
-                                            <label class="form-check-label" for="normalChoice">{{ __('choose-time-for-all-days') }}</label>
-                                        </div>
-
-                                        <div class="form-check form-check-inline">
-                                            <input class="form-check-input" type="radio" name="hours_option" id="customChoice" value="custom">
-                                            <label class="form-check-label" for="customChoice">{{ __('choose-time-for-custom-days') }}</label>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- Hours input for normal choice -->
-                            <div  id="normalChoiceSection">
-
-                                <small class="d-flex m-4">{{ __('time-in-24-h') }}</small>
-
-                                <div class=" d-flex justify-content-between w-100">
-                                    <div class="d-flex justify-content-between align-items-center w-50 mx-5 gap-1">
-                                        <label for="normal_from">{{ __('from') }}</label>
-                                        <input type="text" class="form-control form-control-solid time-24 from"  name="normal_from" id="normal_from" value="09:00"  />
-                                    </div>
-
-                                    <div class="d-flex justify-content-between align-items-center w-50 mx-5 gap-1">
-                                        <label for="normal_to">{{ __('to') }}  </label>
-                                        <input type="text" class="form-control form-control-solid time-24 to"  name="normal_to" id="normal_to" value="20:00"  />
-
-                                    </div>
-                                </div>
-                            </div>
-                            <!--begin::Col-->
-                            <div class="col-md-12 fv-row"  id="customChoiceTabs">
-
-                                <small class="d-flex m-4">{{ __('time-in-24-h') }}</small>
-
-                                <ul class="nav nav-tabs nav-line-tabs mb-5 fs-6 d-flex justify-content-center">
-                                    @foreach([
-                                                'Sunday',
-                                                'Monday',
-                                                'Tuesday',
-                                                'Wednesday',
-                                                'Thursday',
-                                                'Friday',
-                                                'Saturday'
-                                            ] as $idx => $weekDay)
-                                        <li class="nav-item">
-                                            <a class="nav-link text-dark {{$idx == 0 ? 'active' : ''}}" data-bs-toggle="tab" href="#kt_tab_pane_{{8+$idx}}">{{ __(strtolower($weekDay)) }}</a>
-                                        </li>
-                                    @endforeach
-                                </ul>
-
-                                <div class="tab-content" id="customChoiceContent"  >
-
-                                    @foreach([
-                                            'Sunday',
-                                            'Monday',
-                                            'Tuesday',
-                                            'Wednesday',
-                                            'Thursday',
-                                            'Friday',
-                                            'Saturday'
-                                        ] as $idx => $weekDay)
-
-                                        <div class="tab-pane fade show {{$idx == 0 ? 'active' : ''}}" id="kt_tab_pane_{{$idx + 8}}" role="tabpanel">
-                                            <div class=" d-flex justify-content-between w-100">
-                                                <div class="d-flex justify-content-between align-items-center w-50 mx-5 gap-1">
-                                                    <label for="{{strtolower($weekDay)}}_open">{{ __('from') }} </label>
-                                                    <input type="text" class="form-control form-control-solid time-24 from"  name="{{strtolower($weekDay)}}_open" id="{{strtolower($weekDay)}}_open"   />
-                                                </div>
-
-                                                <div class="d-flex justify-content-between align-items-center w-50 mx-5 gap-1">
-                                                    <label for="{{strtolower($weekDay)}}_close">{{ __('to') }} </label>
-                                                    <input type="text" class="form-control form-control-solid time-24 to"  name="{{strtolower($weekDay)}}_close" id="{{strtolower($weekDay)}}_close"   />
-
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                    @endforeach
-
-                                </div>
-                            </div>
-                            <!--end::Col-->
-                        </div>
-                        <!--end::Input group-->
-
-
-                            <!--begin::Actions-->
-                            <div class="text-center">
-                                <button type="reset" id="kt_modal_new_bransh_cancel"
-                                    class="btn btn-light me-3">{{ __('reset') }}</button>
-                                <button type="submit" id="kt_modal_new_bransh_submit" class="btn btn-khardl">
-                                    <span class="indicator-label">{{ __('add-branch') }}</span>
-                                    <span class="indicator-progress">{{ __('please-wait') }}
-                                        <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
-                                </button>
-                            </div>
-
-                        <!--end::Actions-->
-                    </form>
-                    <!--end:Form-->
-                </div>
-                @endif
-
-                <!--end::Modal body-->
-            </div>
-            <!--end::Modal content-->
-        </div>
-        <!--end::Modal dialog-->
-    </div>
-    <!--end::Modal - New Bransh-->
-
 
 
 
@@ -1001,8 +755,6 @@ src="https://goSellJSLib.b-cdn.net/v2.0.0/js/gosell.js"
 
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAzMlj17cdLKcXdS2BlKkl0d31zG04aj2E&v=weekly&libraries=places&loading=async&v=beta" ></script>
 
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.6.13/flatpickr.min.js"></script>
 
 
     <script>
